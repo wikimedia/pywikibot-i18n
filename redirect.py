71623c02d160849155a26ad922252ad11ff90cd7
Merlijn van Deen <valhallasw@users.mediawiki.org>
2011-01-15 13:01:51 +0000

    Moving translatewiki i18n to the rewrite branch. The changes in library/bot code are significant, and thus will not be implemented in trunk.
    https://mediawiki.org/wiki/Special:Code/pywikipedia/8829

---
+++ redirect.py
@@ -0,0 +1,228 @@
+# -*- coding: utf-8 -*-
+msg = {
+	'als': {
+		'redirect-fix-double': u'Bötli: Uflösig vun de doppleti Wyterleitig zue %(to)s',
+		'redirect-broken-redirect-template': u'{{delete}}Wyterleitig wo kaputt isch--[[User:FuzzyBot|FuzzyBot]] 00:52, 15 January 2011 (UTC)',
+		'redirect-remove-broken': u'Wyterleitig wo kaputt isch',
+	},
+	'ar': {
+		'redirect-fix-double': u'روبوت: تصليح تحويلة مزدوجة → %(to)s',
+		'redirect-remove-loop': u'بوت: هدف التحويلة يصنع عقدة تحويل',
+		'redirect-broken-redirect-template': u'{{شطب|تحويلة مكسورة}}',
+		'redirect-remove-broken': u'روبوت: هدف التحويلة غير موجود',
+	},
+	'bar': {
+		'redirect-broken-redirect-template': u'{{delete}}Kaputte Weiterleitung--[[User:FuzzyBot|FuzzyBot]] 00:52, 15 January 2011 (UTC)',
+	},
+	'bat-smg': {
+		'redirect-fix-double': u'Robots: Taisuoms dvėgobs paradresavėms → %(to)s',
+	},
+	'be-x-old': {
+		'redirect-fix-double': u'Робат: выпраўленьне падвойнага перанакіраваньня → %(to)s',
+		'redirect-remove-broken': u'Робат: мэта перанакіраваньня не існуе',
+	},
+	'bjn': {
+		'redirect-fix-double': u'Robot: Pamasangan paugahan ganda ka %(to)s',
+	},
+	'br': {
+		'redirect-fix-double': u'Kempennet adkas doubl gant robot → %(to)s',
+	},
+	'cs': {
+		'redirect-fix-double': u'Robot opravil dvojité přesměrování → %(to)s',
+		'redirect-broken-redirect-template': u'{{smazat|přerušené přesměrování}}',
+		'redirect-remove-broken': u'Přerušené přesměrování',
+	},
+	'de': {
+		'redirect-fix-double': u'Bot: Korrigiere doppelte Weiterleitung zu %(to)s',
+		'redirect-remove-loop': u'Bot: Weiterleitungsziel auf sich selbst',
+		'redirect-broken-redirect-template': u'{{sla|Defekte Weiterleitung --[[User:FuzzyBot|FuzzyBot]] 00:52, 15 January 2011 (UTC)}}',
+		'redirect-remove-broken': u'Bot: Weiterleitungsziel existiert nicht',
+	},
+	'en': {
+		'redirect-fix-double': u'Bot: Fixing double redirect to %(to)s',
+		'redirect-remove-loop': u'[[WP:CSD#G8|G8]]: [[Wikipedia:Redirect|Redirect]] target forms a redirect loop',
+		'redirect-broken-redirect-template': u'{{db-r1}}',
+		'redirect-remove-broken': u'[[WP:CSD#G8|G8]]: [[Wikipedia:Redirect|Redirect]] to a deleted or non-existent page',
+	},
+	'es': {
+		'redirect-fix-double': u'Robot: Arreglando doble redirección → %(to)s',
+		'redirect-remove-broken': u'Robot: La página a la que redirige no existe',
+	},
+	'fa': {
+		'redirect-fix-double': u'ربات:اصلاح تغییر مسیر دوتایی ← %(to)s',
+		'redirect-remove-loop': u'ربات: تغییر مسیر حلقه‌ای',
+		'redirect-broken-redirect-template': u'{{حذف سریع|بن بست}}',
+		'redirect-remove-broken': u'ربات: (بن بست) تغییرمسیر به صفحه‌ای که وجود ندارد',
+	},
+	'fi': {
+		'redirect-fix-double': u'Botti korjasi kaksinkertaisen ohjauksen → %(to)s',
+		'redirect-remove-broken': u'Botti: Ohjauksen kohdesivua ei ole olemassa',
+	},
+	'fr': {
+		'redirect-fix-double': u'Robot: répare double redirection à %(to)s',
+		'redirect-remove-broken': u'Robot : Cible du redirect inexistante',
+	},
+	'frr': {
+		'redirect-fix-double': u'Bot: Ferbeedre dobelt widjerfeerang tu %(to)s',
+		'redirect-remove-loop': u'Bot: Widjerfeerang üüb ham salew',
+		'redirect-broken-redirect-template': u'{{delete|Widjerfeerang uunstaken --[[User:FuzzyBot|FuzzyBot]] 00:52, 15 January 2011 (UTC)}}',
+		'redirect-remove-broken': u'Bot: Widjerfeerang tu en duad sidj.',
+	},
+	'ga': {
+		'redirect-fix-double': u'Róbó: Ag socrú athsheolta dúbailte → %(to)s',
+		'redirect-broken-redirect-template': u'{{scrios|Athsheoladh briste}}',
+		'redirect-remove-broken': u'Róbó : Targaid athsheoladh ar iarraidh',
+	},
+	'he': {
+		'redirect-fix-double': u'בוט: מתקן הפניה כפולה → %(to)s',
+		'redirect-remove-broken': u'בוט: יעד ההפניה אינו קיים',
+	},
+	'hr': {
+		'redirect-fix-double': u'Bot: Popravak dvostrukih preusmjeravanja → %(to)s',
+	},
+	'hu': {
+		'redirect-fix-double': u'Bot: %(to)s lapra mutató dupla átirányítás javítása',
+		'redirect-remove-loop': u'Bot: A cél átirányítási hurkot hoz létre',
+		'redirect-broken-redirect-template': u'{{azonnali|Hibás átirányítás|[[User:FuzzyBot|FuzzyBot]] 00:52, 15 January 2011 (UTC)}}',
+		'redirect-remove-broken': u'Bot: Törölt vagy nemlétező lapra mutató [[WP:REDIR|átirányítás]] törlése',
+	},
+	# Author: McDutchie
+	'ia': {
+		'redirect-fix-double': u'Robot: reparation de duple redirection → %(to)s',
+		'redirect-remove-loop': u'[[WP:CSD#G8|G8]]: Le destination del [[Wikipedia:Redirect|redirection]] forma un circulo de redirectiones',
+		'redirect-broken-redirect-template': u'{{db-r1}}',
+		'redirect-remove-broken': u'[[WP:CSD#G8|G8]]: [[Wikipedia:Redirect|Redirection]] a un pagina delite o non existente',
+	},
+	'is': {
+		'redirect-fix-double': u'Vélmenni: Lagfæri tvöfalda tilvísun → %(to)s',
+	},
+	'it': {
+		'redirect-fix-double': u'Bot: Sistemo i redirect doppi a %(to)s',
+		'redirect-broken-redirect-template': u'{{Cancella subito|9}}',
+		'redirect-remove-broken': u'Bot: Il redirect indirizza ad una pagina inesistente',
+	},
+	'ja': {
+		'redirect-fix-double': u'ロボットによる: 二重リダイレクト修正 → %(to)s',
+		'redirect-remove-broken': u'ロボットによる:リダイレクトの目標は存在しませんでした',
+	},
+	'ka': {
+		'redirect-fix-double': u'რობოტი: ორმაგი გადამისამართების გასწორება → %(to)s',
+		'redirect-remove-broken': u'რობოტი: გადამისამართებული გვერდი არ არსებობს',
+	},
+	'kk': {
+		'redirect-fix-double': u'Бот: Шынжырлы айдатуды түзетті → %(to)s',
+		'redirect-remove-broken': u'Бот: Айдату нысанасы жоқ болды',
+	},
+	'ko': {
+		'redirect-fix-double': u'로봇: 이중 넘겨주기 수정 → %(to)s',
+		'redirect-remove-broken': u'로봇: 끊긴 넘겨주기',
+	},
+	'ksh': {
+		'redirect-fix-double': u'Bot: [[special:doubleredirects|Dubbel Ömlëijdong]] fottjemaat → %(to)s',
+		'redirect-broken-redirect-template': u'{{Schmieß fott}}Di Ömlëijdong jeiht noh nörjendwoh hen.<br />--00:52, 15 January 2011 (UTC)',
+		'redirect-remove-broken': u'Bot: Dė [[Special:BrokenRedirects|Ömlëijdong jingk ennet Liiere]]',
+	},
+	'lb': {
+		'redirect-fix-double': u'Bot: Duebel Viruleedung gefléckt → %(to)s',
+	},
+	'lt': {
+		'redirect-fix-double': u'robotas: Taisomas dvigubas peradresavimas → %(to)s',
+		'redirect-remove-broken': u'robotas: Peradresavimas į niekur',
+	},
+	# Author: Bjankuloski06
+	'mk': {
+		'redirect-fix-double': u'Бот: Исправка на двојни пренасочувања → %(to)s',
+		'redirect-remove-loop': u'[[ВП:КББ|О8]]: Одредницата за [[Википедија:Пренасочување|пренасочување]] образува јамка',
+		'redirect-broken-redirect-template': u'{{db-r1}}',
+		'redirect-remove-broken': u'[[ВП:КББ|О8]]: [[Википедија:Пренасочување|Пренасочување]] кон избришана или непостоечка страница',
+	},
+	'mzn': {
+		'redirect-fix-double': u'ربوت:عوض هایتن دکشیه‌ئون دِتایی → %(to)s',
+		'redirect-remove-broken': u'ربوت:بی‌جاء ِدکشی‌یه‌ئون',
+	},
+	'nds': {
+		'redirect-fix-double': u'Bot: Dubbelte Wiederleiden rutmakt → %(to)s',
+		'redirect-broken-redirect-template': u'{{delete}}Kaputte Wiederleiden, wat nich brukt ward.<br />--[[User:FuzzyBot|FuzzyBot]] 00:52, 15 January 2011 (UTC)',
+		'redirect-remove-broken': u'Bot: Kaputte Wiederleiden ward nich brukt',
+	},
+	# Author: Siebrand
+	'nl': {
+		'redirect-fix-double': u'Robot: dubbele doorverwijzing gecorrigeerd naar %(to)s',
+		'redirect-remove-broken': u'Robot: de doelpagina van de doorverwijzing bestaat niet',
+	},
+	'nn': {
+		'redirect-fix-double': u'robot: retta dobbel omdirigering → %(to)s',
+		'redirect-remove-broken': u'robot: målet for omdirigeringa eksisterer ikkje',
+	},
+	'no': {
+		'redirect-fix-double': u'bot: Retter dobbel omdirigering → %(to)s',
+		'redirect-remove-broken': u'robot: målet for omdirigeringen eksisterer ikke',
+	},
+	'pdc': {
+		'redirect-broken-redirect-template': u'{{lesche|Kaputte Weiderleiding --[[User:FuzzyBot|FuzzyBot]] 00:53, 15 January 2011 (UTC)}}',
+	},
+	'pfl': {
+		'redirect-fix-double': u'Bot: E doppelte Waiterlaitung vabessat zu %(to)s',
+	},
+	'pl': {
+		'redirect-fix-double': u'Robot naprawia podwójne przekierowanie → %(to)s',
+		'redirect-remove-broken': u'Robot: cel przekierowania nie istnieje',
+	},
+	'pt': {
+		'redirect-fix-double': u'Bot: Corrigido duplo redirecionamento → %(to)s',
+		'redirect-remove-broken': u'Bot: Redirecionamento não existe',
+	},
+	'ro': {
+		'redirect-fix-double': u'Robot: Corectarea dublu redirecţionare în %(to)s',
+	},
+	'ru': {
+		'redirect-fix-double': u'Робот: исправление двойного перенаправления → %(to)s',
+		'redirect-broken-redirect-template': u'{{db-redirnone}}',
+		'redirect-remove-broken': u'[[ВП:КБУ#П1|П1]]: перенаправление в никуда',
+	},
+	'sr': {
+		'redirect-fix-double': u'Бот: Поправка дуплих преусмерења → %(to)s',
+		'redirect-remove-broken': u'Бот: Преусмерење не постоји',
+	},
+	'sv': {
+		'redirect-fix-double': u'Robot: Rättar dubbel omdirigering → %(to)s',
+	},
+	'szl': {
+		'redirect-fix-double': u'Robot sprowjo tuplowane przekerowańa → %(to)s',
+	},
+	'th': {
+		'redirect-fix-double': u'โรบอต: แก้หน้าเปลี่ยนทางซ้ำซ้อน → %(to)s',
+		'redirect-remove-broken': u'โรบอต: หน้าเปลี่ยนทางเสีย',
+	},
+	'tr': {
+		'redirect-fix-double': u'Bot değişikliği: Yönlendirmeye olan yönlendirme → %(to)s',
+		'redirect-remove-broken': u'Bot değişikliği: Var olmayan sayfaya olan yönlendirme',
+	},
+	# Author: Тест
+	'uk': {
+		'redirect-fix-double': u'Робот: виправлення подвійного перенаправлення → %(to)s',
+		'redirect-remove-loop': u'[[ВП:КШВ#Перенаправлення|П]]: [[Довідка:Перенаправлення|Перенаправлення]] формує петлю перенаправлень',
+		'redirect-remove-broken': u'[[ВП:КШВ#Перенаправлення|П1]]: [[Довідка:Перенаправлення|Перенаправлення]] на вилучену або неіснуючу сторінку',
+	},
+	'war': {
+		'redirect-fix-double': u'Robot: Gin-ayad in nagduduha nga redirek → %(to)s',
+		'redirect-broken-redirect-template': u'{{delete}}Nautod o nagbinalikbalik nga redirek.--[[User:FuzzyBot|FuzzyBot]] 00:53, 15 January 2011 (UTC)',
+		'redirect-remove-broken': u'Robot: Waray dida an karadto-an han redirek',
+	},
+	'yi': {
+		'redirect-fix-double': u'באט: פארראכטן פארטאפלטע ווייטערפירונג → %(to)s',
+		'redirect-remove-broken': u'באט: ווײַטערפֿירן ציל עקזיסטירט נישט',
+	},
+	'zh': {
+		'redirect-fix-double': u'機器人:修正雙重重定向 → %(to)s',
+		'redirect-broken-redirect-template': u'{{delete|R1}}',
+		'redirect-remove-broken': u'機器人:該重定向的目標不存在',
+	},
+	'zh-classical': {
+		'redirect-fix-double': u'僕:復修渡口 → %(to)s',
+	},
+	'zh-yue': {
+		'redirect-fix-double': u'機械人：拉直連串跳轉 → %(to)s',
+		'redirect-remove-broken': u'機械人：跳轉目標唔存在',
+	},
+}
