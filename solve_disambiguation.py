f74bbaf291c1f30798ad6d0f4bd03621d727604b
Merlijn van Deen <valhallasw@users.mediawiki.org>
2011-01-22 15:16:56 +0000

    Added TranslateWiki support for solve_disambiguation.py
    Due to PYWP-10, I was unable to check the correct working. However, it was
    based on a change in trunk, which worked correctly.
    
    Created during the Amsterdam Hackathon.
    
    https://mediawiki.org/wiki/Special:Code/pywikipedia/8876

---
+++ solve_disambiguation.py
@@ -0,0 +1,651 @@
+# coding=utf-8
+msg = {
+'en': {
+'solve_disambiguation-links-resolved': u'Robot-assisted disambiguation: %(from)s - Changed link(s) to %(to)s',
+'solve_disambiguation-links-removed': u'Robot-assisted disambiguation: %(from)s - Removed link(s).',
+'solve_disambiguation-redirect-resolved': u'Robot-assisted disambiguation: %(from)s - Changed link(s) to %(to)s',
+'solve_disambiguation-redirect-removed': u'Robot-assisted disambiguation: %(from)s - Removed link(s)',
+'solve_disambiguation-adding-dn-template': u'Robot-assisted disambiguation: %(from)s - Marked as needing expert attention',
+'solve_disambiguation-redirect-adding-dn-template': u'Robot-assisted disambiguation: %(from)s - Marked as needing expert attention',
+'solve_disambiguation-unknown-page': u'(unknown)',
+},
+'qqq': {
+'solve_disambiguation-redirect-resolved': u'When working on redirects, there is a possibility to use a different translation than when working on normal disambiguation pages',
+'solve_disambiguation-redirect-removed': u'When working on redirects, there is a possibility to use a different translation than when working on normal disambiguation pages',
+'solve_disambiguation-redirect-adding-dn-template': u'When working on redirects, there is a possibility to use a different translation than when working on normal disambiguation pages',
+'solve_disambiguation-unknown-page': u'If the bot does not know which pages it changed, this will be used instead of the list',
+},
+'de': {
+'solve_disambiguation-links-resolved': u'Bot-unterstützte Begriffsklärung: %(from)s - Link(s) ersetzt durch %(to)s',
+'solve_disambiguation-links-removed': u'Bot-unterstützte Begriffsklärung: %(from)s - Link(s) entfernt',
+'solve_disambiguation-redirect-resolved': u'Bot-unterstützte Redirectauflösung: %(from)s - Link(s) ersetzt durch %(to)s',
+'solve_disambiguation-redirect-removed': u'Bot-unterstützte Redirectauflösung: %(from)s - Link(s) entfernt',
+},
+'fr': {
+'solve_disambiguation-links-resolved': u'Homonymie résolue à l’aide du robot: %(from)s - Modifications du (des) lien(s) pour %(to)s',
+'solve_disambiguation-links-removed': u'Homonymie résolue à l’aide du robot: %(from)s - Retrait du (des) lien(s)',
+'solve_disambiguation-redirect-resolved': u'Correction de lien vers redirect: %(from)s - Modifications du (des) lien(s) pour %(to)s',
+'solve_disambiguation-redirect-removed': u'Correction de lien vers redirect: %(from)s - Retrait du (des) lien(s)',
+},
+'pl': {
+'solve_disambiguation-links-resolved': u'Wspomagane przez robota ujednoznacznienie: %(from)s - Zmieniono link(i) %(to)s',
+'solve_disambiguation-links-removed': u'Wspomagane przez robota ujednoznacznienie: %(from)s - Usunięto link(i)',
+'solve_disambiguation-redirect-resolved': u'Wspomagane przez robota ujednoznacznienie: %(from)s - Zmieniono link(i) %(to)s',
+'solve_disambiguation-redirect-removed': u'Wspomagane przez robota ujednoznacznienie: %(from)s - Usunięto link(i)',
+},
+'it': {
+'solve_disambiguation-links-resolved': u'Sistemazione automatica della disambigua: %(from)s - Inversione di redirect %(to)s',
+'solve_disambiguation-links-removed': u'Sistemazione automatica della disambigua: %(from)s - Collegamenti rimossi',
+'solve_disambiguation-redirect-resolved': u'Sistemazione automatica del redirect: %(from)s - Inversione di redirect %(to)s',
+'solve_disambiguation-redirect-removed': u'Sistemazione automatica del redirect: %(from)s - Collegamenti rimossi',
+},
+'ja': {
+},
+'es': {
+'solve_disambiguation-links-resolved': u'Bot:Desambiguación asistida: %(from)s - Cambiando enlace(s) para %(to)s',
+},
+'pt': {
+'solve_disambiguation-links-resolved': u'Desambiguação assistida por bot: %(from)s link(s) mudado(s) para %(to)s',
+'solve_disambiguation-links-removed': u'Desambiguação assistida por bot: %(from)s link(s) removido(s)',
+'solve_disambiguation-redirect-resolved': u'Desambiguação assistida por bot: %(from)s link(s) mudados para %(to)s',
+'solve_disambiguation-redirect-removed': u'Desambiguação assistida por bot: %(from)s link(s) removidos',
+'solve_disambiguation-unknown-page': u'(desconhecido)',
+},
+'nl': {
+'solve_disambiguation-links-resolved': u'Botgeholpen doorverwijzing: [[%(from)s]] - Verwijzing(en) gewijzigd naar %(to)s',
+'solve_disambiguation-links-removed': u'Botgeholpen doorverwijzing: [[%(from)s]] - Verwijzing(en) verwijderd',
+'solve_disambiguation-redirect-resolved': u'Botgeholpen oplossing voor doorverwijzing: [[%(from)s]] - Verwijzing(en) gewijzigd naar %(to)s',
+'solve_disambiguation-redirect-removed': u'Botgeholpen oplossing voor doorverwijzing: [[%(from)s]] - Verwijzing(en) verwijderd',
+},
+'ru': {
+'solve_disambiguation-links-resolved': u'Разрешение значений с помощью бота: %(from)s - Changed link(s) to %(to)s',
+'solve_disambiguation-links-removed': u'Разрешение значений с помощью бота: %(from)s - Removed link(s)',
+'solve_disambiguation-redirect-resolved': u'Разрешение значений с помощью бота: %(from)s - Changed link(s) to %(to)s',
+'solve_disambiguation-redirect-removed': u'Разрешение значений с помощью бота: %(from)s - Removed link(s)',
+},
+'sv': {
+'solve_disambiguation-links-resolved': u'Länkar direkt till rätt artikel för: %(from)s - Bytte länk(ar) till %(to)s',
+'solve_disambiguation-links-removed': u'Länkar direkt till rätt artikel för: %(from)s - Tog bort länk(ar)',
+'solve_disambiguation-redirect-resolved': u'Länkar direkt till rätt artikel för: %(from)s - Bytte länk(ar) till %(to)s',
+'solve_disambiguation-redirect-removed': u'Länkar direkt till rätt artikel för: %(from)s - Tog bort länk(ar)',
+},
+'zh': {
+},
+'ca': {
+'solve_disambiguation-links-resolved': u'Bot:Desambiguació assistida: %(from)s - Canviant enllaç(os) per %(to)s',
+'solve_disambiguation-links-removed': u'Desambiguació assistida: %(from)s - Eliminant enllaç(os).',
+'solve_disambiguation-redirect-resolved': u'Desambiguació assistida: %(from)s - Canviant enllaç(os) a %(to)s',
+'solve_disambiguation-redirect-removed': u'Desambiguació assistida: %(from)s - Eliminant enllaç(os)',
+'solve_disambiguation-unknown-page': u'(Canvi manual)',
+},
+'no': {
+'solve_disambiguation-links-resolved': u'bot: Retter lenke til peker: %(from)s - Endret lenke(r) til %(to)s',
+'solve_disambiguation-links-removed': u'bot: Retter lenke til peker: %(from)s - Fjernet lenke(r)',
+'solve_disambiguation-redirect-resolved': u'bot: Endrer omdirigeringslenke: %(from)s - Endret lenke(r) til %(to)s',
+'solve_disambiguation-redirect-removed': u'bot: Endrer omdirigeringslenke: %(from)s - Fjernet lenke(r)',
+},
+'fi': {
+'solve_disambiguation-links-resolved': u'Täsmennystä botin avulla: %(from)s korvattiin link(e)illä %(to)s',
+'solve_disambiguation-links-removed': u'Täsmennystä botin avulla: %(from)s - poistettiin linkkejä.',
+'solve_disambiguation-redirect-resolved': u'Täsmennystä botin avulla: %(from)s korvattiin link(e)illä %(to)s',
+'solve_disambiguation-redirect-removed': u'Täsmennystä botin avulla: %(from)s - poistettiin linkkejä',
+'solve_disambiguation-unknown-page': u'(tuntematon)',
+},
+'uk': {
+'solve_disambiguation-links-resolved': u'Виправлення посилання на багатозначність за допомогою бота: %(from)s змінено на %(to)s',
+'solve_disambiguation-links-removed': u'Виправлення посилання на багатозначність за допомогою бота: %(from)s вилучено',
+'solve_disambiguation-redirect-resolved': u'Виправлення посилання на багатозначність за допомогою бота: %(from)s змінено на %(to)s',
+'solve_disambiguation-redirect-removed': u'Виправлення посилання на багатозначність за допомогою бота: %(from)s вилучено',
+},
+'cs': {
+'solve_disambiguation-links-resolved': u'Odstranění linku na rozcestník [[%(from)s]] s použitím robota - Změněn(y) odkaz(y) na %(to)s',
+'solve_disambiguation-links-removed': u'Odstranění linku na rozcestník [[%(from)s]] s použitím robota - Odstraněn(y) odkaz(y)',
+'solve_disambiguation-redirect-resolved': u'Robot opravil přesměrování na %(from)s - Změněn(y) odkaz(y) na %(to)s',
+'solve_disambiguation-redirect-removed': u'Robot opravil přesměrování na %(from)s - Odstraněn(y) odkaz(y)',
+},
+'hu': {
+'solve_disambiguation-links-resolved': u'Bottal végzett egyértelműsítés: %(from)s –> %(to)s',
+'solve_disambiguation-links-removed': u'Bottal végzett egyértelműsítés: %(from)s – hivatkozások eltávolítása',
+'solve_disambiguation-redirect-resolved': u'Bottal végzett egyértelműsítés: %(from)s –> %(to)s',
+'solve_disambiguation-redirect-removed': u'Bottal támogatott egyértelműsítés: %(from)s – hivatkozások eltávolítása',
+'solve_disambiguation-unknown-page': u'(ismeretlen)',
+},
+'ro': {
+},
+'ko': {
+'solve_disambiguation-links-resolved': u'로봇의 도움을 받아 동음이의 처리 : [[%(from)s]] - %(to)s 문서로 링크 걸음',
+'solve_disambiguation-links-removed': u'로봇의 도움을 받아 동음이의 처리: [[%(from)s]] - 링크 제거',
+'solve_disambiguation-redirect-resolved': u'로봇의 도움을 받아 동음이의 처리: [[%(from)s]] - %(to)s 문서로 링크 걸음',
+'solve_disambiguation-redirect-removed': u'로봇의 도움을 받아 동음이의 처리: [[%(from)s]] - 링크 제거',
+},
+'tr': {
+},
+'vi': {
+},
+'id': {
+},
+'da': {
+'solve_disambiguation-links-resolved': u'Retter flertydigt link til: %(from)s - Ændrede link(s) til %(to)s',
+'solve_disambiguation-links-removed': u'Retter flertydigt link til: %(from)s - Fjernede link(s)',
+'solve_disambiguation-redirect-resolved': u'Retter flertydigt link til: %(from)s - Ændrede link(s) til %(to)s',
+'solve_disambiguation-redirect-removed': u'Retter flertydigt link til: %(from)s - Fjernede link(s)',
+},
+'ar': {
+'solve_disambiguation-links-resolved': u'توضيح بمساعدة روبوت: %(from)s - غير الوصلة أو الوصلات إلى %(to)s',
+'solve_disambiguation-links-removed': u'توضيح بمساعدة روبوت: %(from)s - أزال الوصلة أو الوصلات.',
+'solve_disambiguation-redirect-resolved': u'توضيح بمساعدة روبوت: %(from)s - غير الوصلة أو الوصلات إلى %(to)s',
+'solve_disambiguation-redirect-removed': u'توضيح بمساعدة روبوت: %(from)s - أزال الوصلة أو الوصلات',
+'solve_disambiguation-adding-dn-template': u'توضيح بمساعدة روبوت: %(from)s - التعليم كمحتاجة لانتباه خبير',
+'solve_disambiguation-redirect-adding-dn-template': u'توضيح بمساعدة روبوت: %(from)s - التعليم كمحتاجة لانتباه خبير',
+'solve_disambiguation-unknown-page': u'(غير معروف)',
+},
+'eo': {
+},
+'sr': {
+'solve_disambiguation-links-resolved': u'Решавање вишезначних одредница помоћу бота: %(from)s - Changed link(s) to %(to)s',
+'solve_disambiguation-links-removed': u'Решавање вишезначних одредница помоћу бота: %(from)s - Removed link(s)',
+'solve_disambiguation-redirect-resolved': u'Решавање вишезначних одредница помоћу бота: %(from)s - Changed link(s) to %(to)s',
+'solve_disambiguation-redirect-removed': u'Решавање вишезначних одредница помоћу бота: %(from)s - Removed link(s)',
+},
+'lt': {
+'solve_disambiguation-links-resolved': u'Nuorodų į nukrepiamąjį straipsnį keitimas: %(from)s - Pakeistos nuorodos į %(to)s',
+'solve_disambiguation-links-removed': u'Nuorodų į nukrepiamąjį straipsnį keitimas: %(from)s - Pašalintos nuorodos',
+'solve_disambiguation-redirect-resolved': u'Nuorodų į peradresavimo straipsnį keitimas: %(from)s - Pakeistos nuorodos į %(to)s',
+'solve_disambiguation-redirect-removed': u'Nuorodų į peradresavimo straipsnį keitimas: %(from)s - Pašalintos nuorodos',
+},
+'sk': {
+},
+'vo': {
+},
+'fa': {
+'solve_disambiguation-links-resolved': u'ابهام زدایی به کمک ربات: %(from)s - پیوند به [[%(to)s]]',
+'solve_disambiguation-links-removed': u'ابهام زدایی به کمک ربات: حذف %(from)s',
+'solve_disambiguation-redirect-resolved': u'ابهام زدایی به کمک ربات: %(from)s - پیوند به [[%(to)s]]',
+'solve_disambiguation-redirect-removed': u'ابهام زدایی به کمک ربات: حذف %(from)s',
+'solve_disambiguation-unknown-page': u'(نامعلوم)',
+},
+'he': {
+'solve_disambiguation-links-resolved': u'תיקון קישור לדף פירושונים באמצעות בוט: %(from)s',
+'solve_disambiguation-links-removed': u'הסרת קישור לדף פירושונים באמצעות בוט: %(from)s',
+'solve_disambiguation-redirect-resolved': u'תיקון קישור לדף פירושונים באמצעות בוט: %(from)s שונה ל%(to)s',
+'solve_disambiguation-redirect-removed': u'הסרת קישור לדף פירושונים באמצעות בוט: %(from)s',
+},
+'bg': {
+},
+'sl': {
+},
+'ms': {
+},
+'war': {
+},
+'hr': {
+},
+'et': {
+},
+'new': {
+},
+'simple': {
+},
+'hi': {
+},
+'gl': {
+},
+'th': {
+},
+'eu': {
+},
+'nn': {
+},
+'roa-rup': {
+},
+'el': {
+},
+'ht': {
+},
+'az': {
+},
+'tl': {
+},
+'la': {
+},
+'te': {
+},
+'ka': {
+},
+'mk': {
+},
+'ceb': {
+},
+'sh': {
+},
+'br': {
+},
+'pms': {
+},
+'mr': {
+},
+'lv': {
+},
+'lb': {
+},
+'jv': {
+},
+'be-x-old': {
+},
+'cy': {
+},
+'is': {
+},
+'bs': {
+},
+'sq': {
+},
+'ta': {
+},
+'be': {
+},
+'bpy': {
+},
+'an': {
+},
+'oc': {
+},
+'bn': {
+},
+'sw': {
+},
+'io': {
+},
+'lmo': {
+},
+'fy': {
+},
+'gu': {
+},
+'af': {
+},
+'nds': {
+},
+'ml': {
+},
+'qu': {
+},
+'scn': {
+},
+'ur': {
+},
+'ku': {
+},
+'zh-yue': {
+},
+'su': {
+},
+'ast': {
+},
+'nap': {
+},
+'bat-smg': {
+},
+'hy': {
+},
+'yo': {
+},
+'ga': {
+},
+'cv': {
+},
+'wa': {
+},
+'ne': {
+},
+'ksh': {
+},
+'pnb': {
+},
+'kn': {
+},
+'tg': {
+},
+'roa-tara': {
+},
+'vec': {
+},
+'yi': {
+},
+'gd': {
+},
+'tt': {
+},
+'zh-min-nan': {
+},
+'os': {
+},
+'uz': {
+},
+'als': {
+},
+'pam': {
+},
+'sah': {
+},
+'kk': {
+'solve_disambiguation-links-resolved': u'Айрықты мағыналарды бот көмегімен шешу: %(from)s - Changed link(s) to %(to)s',
+'solve_disambiguation-links-removed': u'Айрықты мағыналарды бот көмегімен шешу: %(from)s - Removed link(s).',
+'solve_disambiguation-redirect-resolved': u'Айрықты мағыналарды бот көмегімен шешу: %(from)s - Changed link(s) to %(to)s',
+'solve_disambiguation-redirect-removed': u'Айрықты мағыналарды бот көмегімен шешу: %(from)s - Removed link(s).',
+},
+'arz': {
+},
+'mi': {
+},
+'li': {
+},
+'am': {
+},
+'nah': {
+},
+'hsb': {
+},
+'glk': {
+},
+'co': {
+},
+'gan': {
+},
+'mn': {
+},
+'sco': {
+},
+'ia': {
+'solve_disambiguation-links-resolved': u'Disambiguation assistite per robot: %(from)s - Changed link(s) to %(to)s',
+'solve_disambiguation-links-removed': u'Disambiguation assistite per robot: %(from)s - Removed link(s).',
+'solve_disambiguation-redirect-resolved': u'Resolution de redirectiones assistite per robot: %(from)s - Changed link(s) to %(to)s',
+'solve_disambiguation-redirect-removed': u'Resolution de redirectiones assistite per robot: %(from)s - Removed link(s).',
+},
+'bcl': {
+},
+'my': {
+},
+'fo': {
+},
+'fiu-vro': {
+},
+'nds-nl': {
+},
+'si': {
+},
+'tk': {
+},
+'vls': {
+},
+'sa': {
+},
+'bar': {
+},
+'mg': {
+},
+'gv': {
+},
+'ilo': {
+},
+'dv': {
+},
+'nrm': {
+},
+'pag': {
+},
+'map-bms': {
+},
+'ckb': {
+},
+'rm': {
+},
+'diq': {
+},
+'se': {
+},
+'mzn': {
+},
+'wuu': {
+},
+'fur': {
+},
+'ug': {
+},
+'lij': {
+},
+'mt': {
+},
+'bh': {
+},
+'nov': {
+},
+'bo': {
+},
+'ang': {
+},
+'csb': {
+},
+'sc': {
+},
+'zh-classical': {
+},
+'hif': {
+},
+'km': {
+},
+'lad': {
+},
+'cbk-zam': {
+},
+'pi': {
+},
+'frp': {
+},
+'ps': {
+},
+'hak': {
+},
+'kw': {
+},
+'pa': {
+},
+'nv': {
+},
+'szl': {
+},
+'xal': {
+},
+'pdc': {
+},
+'haw': {
+},
+'stq': {
+},
+'ie': {
+},
+'so': {
+},
+'kv': {
+},
+'crh': {
+},
+'to': {
+},
+'ace': {
+},
+'mhr': {
+},
+'pcd': {
+},
+'myv': {
+},
+'krc': {
+},
+'ln': {
+},
+'ext': {
+},
+'gn': {
+},
+'ky': {
+},
+'eml': {
+},
+'rw': {
+},
+'arc': {
+},
+'pap': {
+},
+'ay': {
+},
+'jbo': {
+},
+'wo': {
+},
+'bjn': {
+},
+'ba': {
+},
+'frr': {
+},
+'ce': {
+},
+'kl': {
+},
+'tpi': {
+},
+'udm': {
+},
+'kab': {
+},
+'ty': {
+},
+'srn': {
+},
+'zea': {
+},
+'mrj': {
+},
+'gag': {
+},
+'koi': {
+},
+'dsb': {
+},
+'ab': {
+},
+'ig': {
+},
+'or': {
+},
+'lo': {
+},
+'kg': {
+},
+'av': {
+},
+'mdf': {
+},
+'mwl': {
+},
+'rmy': {
+},
+'cu': {
+},
+'kaa': {
+},
+'sm': {
+},
+'tet': {
+},
+'got': {
+},
+'ks': {
+},
+'bm': {
+},
+'ik': {
+},
+'sd': {
+},
+'bi': {
+},
+'lbe': {
+},
+'na': {
+},
+'pih': {
+},
+'iu': {
+},
+'pnt': {
+},
+'as': {
+},
+'chr': {
+},
+'cdo': {
+},
+'ss': {
+},
+'pfl': {
+},
+'ha': {
+},
+'ee': {
+},
+'ti': {
+},
+'om': {
+},
+'zu': {
+},
+'za': {
+},
+'ve': {
+},
+'ts': {
+},
+'rn': {
+},
+'dz': {
+},
+'sg': {
+},
+'cr': {
+},
+'ch': {
+},
+'lg': {
+},
+'bug': {
+},
+'bxr': {
+},
+'fj': {
+},
+'ny': {
+},
+'ak': {
+},
+'st': {
+},
+'xh': {
+},
+'tn': {
+},
+'ff': {
+},
+'ki': {
+},
+'tum': {
+},
+'sn': {
+},
+'tw': {
+},
+'chy': {
+},
+}
