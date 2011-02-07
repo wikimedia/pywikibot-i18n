ea96bc8ed8fff0ca721bdd060952a7b2e9105790
xqt <xqt@users.mediawiki.org>
2011-02-07 14:43:22 +0000

    i18n for cosmetic_changes
    https://mediawiki.org/wiki/Special:Code/pywikipedia/8937

---
+++ cosmetic_changes.py
@@ -0,0 +1,239 @@
+# -*- coding: utf-8 -*-
+msg = {
+	'als': {
+		'cosmetic_changes-standalone': u'Bötli: chleineri Änderige',
+		'cosmetic_changes-append': u'; chleineri Änderige',
+	},
+	'ar': {
+		'cosmetic_changes-standalone': u'روبوت: تغييرات تجميلية',
+		'cosmetic_changes-append': u'; تغييرات تجميلية',
+	},
+	'be-x-old': {
+		'cosmetic_changes-standalone': u'Робат: касмэтычныя зьмены',
+		'cosmetic_changes-append': u'; касмэтычныя зьмены',
+	},
+	'bg': {
+		'cosmetic_changes-standalone': u'Робот козметични промени',
+		'cosmetic_changes-append': u'; козметични промени',
+	},
+	'br': {
+		'cosmetic_changes-standalone': u'Bot: Kemm dister',
+		'cosmetic_changes-append': u'; Kemm dister',
+	},
+	'ca': {
+		'cosmetic_changes-standalone': u'Robot: Canvis cosmètics',
+		'cosmetic_changes-append': u'; canvis cosmètics',
+	},
+	'ckb': {
+		'cosmetic_changes-standalone': u'بۆت: دەستکاریی جوانکاری',
+		'cosmetic_changes-append': u'; دەستکاریی جوانکاری',
+	},
+	'commons': {
+		'cosmetic_changes-standalone': u'Bot: [[Commons talk:Tools/pywiki file description cleanup|desc page fmt]]',
+		'cosmetic_changes-append': u'; [[Commons talk:Tools/pywiki file description cleanup|desc page fmt]]',
+	},
+	'cs': {
+		'cosmetic_changes-standalone': u'Robotické: kosmetické úpravy',
+		'cosmetic_changes-append': u'; kosmetické úpravy',
+	},
+	'da': {
+		'cosmetic_changes-standalone': u'Bot: Kosmetiske ændringer',
+		'cosmetic_changes-append': u'; kosmetiske ændringer',
+	},
+	'de': {
+		'cosmetic_changes-standalone': u'Bot: Kosmetische Änderungen',
+		'cosmetic_changes-append': u'; kosmetische Änderungen',
+	},
+	'el': {
+		'cosmetic_changes-standalone': u'Ρομπότ: διακοσμητικές αλλαγές',
+		'cosmetic_changes-append': u'; διακοσμητικές αλλαγές',
+	},
+	'en': {
+		'cosmetic_changes-standalone': u'Robot: Cosmetic changes',
+		'cosmetic_changes-append': u'; cosmetic changes',
+	},
+	'es': {
+		'cosmetic_changes-standalone': u'Robot: Cambios triviales',
+		'cosmetic_changes-append': u'; cambios triviales',
+	},
+	'et': {
+		'cosmetic_changes-standalone': u'robot: kosmeetilised muudatused',
+		'cosmetic_changes-append': u'; kosmeetilised muudatused',
+	},
+	'fa': {
+		'cosmetic_changes-standalone': u'ربات: زیباسازی',
+		'cosmetic_changes-append': u'; زیباسازی',
+	},
+	'fi': {
+		'cosmetic_changes-standalone': u'Botti kosmeettisia muutoksia',
+		'cosmetic_changes-append': u'; kosmeettisia muutoksia',
+	},
+	'fr': {
+		'cosmetic_changes-standalone': u'Robot : Changement de type cosmétique',
+		'cosmetic_changes-append': u'; changement de type cosmétique',
+	},
+	'frr': {
+		'cosmetic_changes-standalone': u'Bot: Kosmeetisk feranerangen',
+		'cosmetic_changes-append': u'; kosmeetisk feranerangen',
+	},
+	'fy': {
+		'cosmetic_changes-standalone': u'bot tekstwiziging',
+		'cosmetic_changes-append': u'; tekstwiziging',
+	},
+	'gl': {
+		'cosmetic_changes-standalone': u'bot Cambios estética',
+		'cosmetic_changes-append': u'; cambios estética',
+	},
+	'he': {
+		'cosmetic_changes-standalone': u'בוט: שינויים קוסמטיים',
+		'cosmetic_changes-append': u'; שינויים קוסמטיים',
+	},
+	'hi': {
+		'cosmetic_changes-standalone': u'Bot: अंगराग परिवर्तन',
+		'cosmetic_changes-append': u'; अंगराग परिवर्तन',
+	},
+	'hr': {
+		'cosmetic_changes-standalone': u'robot kozmetičke promjene',
+		'cosmetic_changes-append': u'; kozmetičke promjene',
+	},
+	'hu': {
+		'cosmetic_changes-standalone': u'Bot: kozmetikai változtatások',
+		'cosmetic_changes-append': u'; kozmetikai változtatások',
+	},
+	'ia': {
+		'cosmetic_changes-standalone': u'Robot: Cambios cosmetic',
+		'cosmetic_changes-append': u'; cambios cosmetic',
+	},
+	'id': {
+		'cosmetic_changes-standalone': u'bot kosmetik perubahan',
+		'cosmetic_changes-append': u'; kosmetik perubahan',
+	},
+	'it': {
+		'cosmetic_changes-standalone': u'Bot: Modifiche estetiche',
+		'cosmetic_changes-append': u'; modifiche estetiche',
+	},
+	'ja': {
+		'cosmetic_changes-standalone': u'ロボットによる: 細部の編集',
+		'cosmetic_changes-append': u'; 細部の編集',
+	},
+	'ko': {
+		'cosmetic_changes-standalone': u'로봇: 예쁘게 바꿈',
+		'cosmetic_changes-append': u'; 예쁘게 바꿈',
+	},
+	'la': {
+		'cosmetic_changes-standalone': u'automaton: mutationes minores',
+		'cosmetic_changes-append': u'; mutationes minores',
+	},
+	'lt': {
+		'cosmetic_changes-standalone': u'robotas: smulkūs taisymai',
+		'cosmetic_changes-append': u'; smulkūs taisymai',
+	},
+	'lv': {
+		'cosmetic_changes-standalone': u'robots kosmētiskās izmaiņas',
+		'cosmetic_changes-append': u'; kosmētiskās izmaiņas',
+	},
+	'mk': {
+		'cosmetic_changes-standalone': u'Бот: козметички промени',
+		'cosmetic_changes-append': u'; козметички промени',
+	},
+	'ms': {
+		'cosmetic_changes-standalone': u'Bot: perubahan kosmetik',
+		'cosmetic_changes-append': u'; perubahan kosmetik',
+	},
+	'mt': {
+		'cosmetic_changes-standalone': u'Bot: kosmetiċi bidliet',
+		'cosmetic_changes-append': u'; kosmetiċi bidliet',
+	},
+	'nl': {
+		'cosmetic_changes-standalone': u'Bot: cosmetische wijzigingen',
+		'cosmetic_changes-append': u'; cosmetische veranderingen',
+	},
+	'nn': {
+		'cosmetic_changes-standalone': u'Robot: Kosmetiske endringar',
+		'cosmetic_changes-append': u'; kosmetiske endringar',
+	},
+	'no': {
+		'cosmetic_changes-standalone': u'Bot: Kosmetiske endringer',
+		'cosmetic_changes-append': u'; kosmetiske endringer',
+	},
+	'pdc': {
+		'cosmetic_changes-standalone': u'Waddefresser: gleenere Enneringe',
+		'cosmetic_changes-append': u'; gleenere Enneringe',
+	},
+	'pfl': {
+		'cosmetic_changes-standalone': u'Bot: Klännere Ännerunge',
+		'cosmetic_changes-append': u'; klännere Ännerunge',
+	},
+	'pl': {
+		'cosmetic_changes-standalone': u'Robot dokonuje poprawek kosmetycznych',
+		'cosmetic_changes-append': u'; zmiany kosmetyczne',
+	},
+	'pt': {
+		'cosmetic_changes-standalone': u'Bot: Mudanças triviais',
+		'cosmetic_changes-append': u'; mudanças triviais',
+	},
+	'ro': {
+		'cosmetic_changes-standalone': u'robot modificări cosmetice',
+		'cosmetic_changes-append': u'; modificări cosmetice',
+	},
+	'ru': {
+		'cosmetic_changes-standalone': u'робот косметические изменения',
+		'cosmetic_changes-append': u'; косметические изменения',
+	},
+	'sk': {
+		'cosmetic_changes-standalone': u'robot kozmetické zmeny',
+		'cosmetic_changes-append': u'; kozmetické zmeny',
+	},
+	'sl': {
+		'cosmetic_changes-standalone': u'robot kozmetične spremembe',
+		'cosmetic_changes-append': u'; kozmetične spremembe',
+	},
+	'sr': {
+		'cosmetic_changes-standalone': u'Бот козметичке промене',
+		'cosmetic_changes-append': u'; козметичке промене',
+	},
+	'sv': {
+		'cosmetic_changes-standalone': u'Bot: Kosmetiska ändringar',
+		'cosmetic_changes-append': u'; kosmetiska ändringar',
+	},
+	'th': {
+		'cosmetic_changes-standalone': u'บอต ปรับแต่งให้อ่านง่าย',
+		'cosmetic_changes-append': u'; ปรับแต่งให้อ่านง่าย',
+	},
+	'tk': {
+		'cosmetic_changes-standalone': u'Bot: kosmetik üýtgeşme',
+		'cosmetic_changes-append': u'; kosmetik üýtgeşme',
+	},
+	'tl': {
+		'cosmetic_changes-standalone': u'robot Kosmetiko pagbabago',
+		'cosmetic_changes-append': u'; Kosmetiko pagbabago',
+	},
+	'tr': {
+		'cosmetic_changes-standalone': u'Bot Kozmetik değişiklikler',
+		'cosmetic_changes-append': u'; Kozmetik değişiklikler',
+	},
+	'uk': {
+		'cosmetic_changes-standalone': u'робот косметичні зміни',
+		'cosmetic_changes-append': u'; косметичні зміни',
+	},
+	'vec': {
+		'cosmetic_changes-standalone': u'Bot: Modifiche estetiche',
+		'cosmetic_changes-append': u'; modifiche estetiche',
+	},
+	'vi': {
+		'cosmetic_changes-standalone': u'robot: Sửa cách trình bày',
+		'cosmetic_changes-append': u'; sửa cách trình bày',
+	},
+	'war': {
+		'cosmetic_changes-standalone': u'Robot: Kosmetiko nga mga pagbag-o',
+		'cosmetic_changes-append': u'; kosmetiko nga mga pagbag-o',
+	},
+	'yi': {
+		'cosmetic_changes-standalone': u'באט: קאסמעטישע ענדערונגען',
+		'cosmetic_changes-append': u'; קאסמעטישע ענדערונגען',
+	},
+	'zh': {
+		'cosmetic_changes-standalone': u'機器人: 細部更改',
+		'cosmetic_changes-append': u'; 細部更改',
+	},
+}
