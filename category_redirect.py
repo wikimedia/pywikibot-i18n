c7d8783768fa1daf3be46bd2887d342c8824ff9e
xqt <xqt@users.mediawiki.org>
2011-02-15 20:47:28 +0000

    i18n tw support for category_redirect.
    https://mediawiki.org/wiki/Special:Code/pywikipedia/8960

---
+++ category_redirect.py
@@ -0,0 +1,246 @@
+# -*- coding: utf-8 -*-
+msg = {
+	'af': {
+		'category_redirect-fix-double': u'Robot: dubbele aanstuur reggemaak',
+	},
+	'als': {
+		'category_redirect-fix-double': u'Bötli: Uflösig vun de doppleti Wyterleitig',
+	},
+	'bat-smg': {
+		'category_redirect-fix-double': u'Robots: Taisuoms dvėgobs paradresavėms',
+	},
+	'be-tarask': {
+		'category_redirect-fix-double': u'Робат: выпраўленьне падвойнага перанакіраваньня',
+	},
+	'be-x-old': {
+		'category_redirect-fix-double': u'Робат: выпраўленьне падвойнага перанакіраваньня',
+	},
+	'bjn': {
+		'category_redirect-fix-double': u'Robot: Pamasangan paugahan ganda',
+	},
+	'br': {
+		'category_redirect-fix-double': u'Kempennet adkas doubl gant robot',
+	},
+	'bs': {
+		'category_redirect-fix-double': u'Bot: Popravlja dvostruka preusmjerenja',
+	},
+	'cs': {
+		'category_redirect-fix-double': u'Robot opravil dvojité přesměrování',
+	},
+	'de': {
+		'category_redirect-fix-double': u'Bot: Korrigiere doppelte Weiterleitung',
+	},
+	'en': {
+		'category_redirect-change-category': u'Robot: change redirected category [[:%(oldCatLink)s|%(oldCatTitle)s]] to [[:%(newCatLink)s|%(newCatTitle)s]]',
+		'category_redirect-add-template': u'Robot: adding category redirect template for maintenance',
+		'category_redirect-fix-double': u'Bot: fixing double-redirect',
+		'category_redirect-comment': u'Category redirect maintenance bot',
+		'category_redirect-edit-request-item': u'* %(title)s is in %(oldcat)s, which is a redirect to %(newcat)s',
+		'category_redirect-edit-request': u'The following protected pages have been detected as requiring updates to category links: %(itemlist)s',
+	},
+	'eo': {
+		'category_redirect-fix-double': u'Roboto: Riparis duoblan alidirekton',
+	},
+	'es': {
+		'category_redirect-change-category': u'Bot: moviendo páginas de categoría redirigida',
+		'category_redirect-add-template': u'Bot: añadiendo plantilla de categoría redirigida para mantenimiento',
+		'category_redirect-fix-double': u'Bot: Arreglando doble redirección',
+		'category_redirect-comment': u'Bot de mantenimento de categorías redirigidas',
+		'category_redirect-edit-request-item': u'* %(title)s está en %(oldcat)s, el cual redirecciona a %(newcat)s',
+		'category_redirect-edit-request': u'Se han detectado las siguientes páginas protegidas y se requieren actualizaciones de enlaces de categorías: %(itemlist)s',
+	},
+	'fa': {
+		'category_redirect-change-category': u'ربات:تغییر رده‌هایی که انتقال یافته‌اند',
+		'category_redirect-add-template': u'ربات:افزودن الگوی رده بهتر',
+		'category_redirect-fix-double': u'ربات:تصحیح تغییرمسیرهای دوتایی',
+		'category_redirect-comment': u'ربات:مرتب‌سازی رده‌های منتقل‌شده',
+		'category_redirect-edit-request-item': u'* %(title)s در %(oldcat)s قرار دارد،که به %(newcat)s انتقال یافته‌است.',
+		'category_redirect-edit-request': u'صفحات حفاظت‌شده زیر نیاز به بروزرسانی دارند صفحات: %(itemlist)s',
+	},
+	'fr': {
+		'category_redirect-fix-double': u'Robot: répare double redirection',
+	},
+	'frp': {
+		'category_redirect-fix-double': u'Bot : rèpâre redirèccion dobla',
+	},
+	'frr': {
+		'category_redirect-fix-double': u'Bot: Ferbeedre dobelt widjerfeerang',
+	},
+	'fur': {
+		'category_redirect-fix-double': u'Robot: o comedi un re-indreçament dopli',
+	},
+	'ga': {
+		'category_redirect-fix-double': u'Róbó: Ag socrú athsheolta dúbailte',
+	},
+	'gl': {
+		'category_redirect-fix-double': u'Bot: Arranxo a redirección dobre cara',
+	},
+	'gsw': {
+		'category_redirect-fix-double': u'Bötli: Uflösig vun de doppleti Wyterleitig',
+	},
+	'he': {
+		'category_redirect-fix-double': u'בוט: מתקן הפניה כפולה',
+	},
+	'hr': {
+		'category_redirect-fix-double': u'Bot: Popravak dvostrukih preusmjeravanja',
+	},
+	'ia': {
+		'category_redirect-fix-double': u'Robot: reparation de duple redirection',
+	},
+	'id': {
+		'category_redirect-fix-double': u'Bot: Memperbaiki pengalihan ganda',
+	},
+	'is': {
+		'category_redirect-fix-double': u'Vélmenni: Lagfæri tvöfalda tilvísun',
+	},
+	'it': {
+		'category_redirect-fix-double': u'Bot: Sistemo i redirect doppi',
+	},
+	'ja': {
+		'category_redirect-fix-double': u'ロボットによる: 二重リダイレクト修正',
+	},
+	'ka': {
+		'category_redirect-fix-double': u'რობოტი: ორმაგი გადამისამართების გასწორება',
+	},
+	'kk': {
+		'category_redirect-fix-double': u'Бот: Шынжырлы айдатуды түзетті',
+	},
+	'ko': {
+		'category_redirect-fix-double': u'로봇: 이중 넘겨주기 수정',
+	},
+	'ksh': {
+		'category_redirect-change-category': u'Bot: Sigk uß en ömjeleidt Saachjropp eruß jesammdt.',
+		'category_redirect-add-template': u'Bot: Ömleidungsschalbon dobeijedonn.',
+		'category_redirect-fix-double': u'Bot: Dubbel Ömlëijdong fottjemaat.',
+		'category_redirect-comment': u'Bot för de Saachjroppe ier Ömleidunge.',
+		'category_redirect-edit-request-item': u'* %(title)s es en %(oldcat)s, un dat es en Ömleidung op %(newcat)s',
+		'category_redirect-edit-request': u'Hee di Sigge sin jeschötz un möße ier Saachjroppe odder Lingks op Saachjroppe aanjepaß krijje: %(itemlist)s',
+	},
+	'la': {
+		'category_redirect-fix-double': u'automaton: rectificatio redirectionis duplicis',
+	},
+	'lb': {
+		'category_redirect-fix-double': u'Bot: Duebel Viruleedung gefléckt',
+	},
+	'lt': {
+		'category_redirect-fix-double': u'robotas: Taisomas dvigubas peradresavimas',
+	},
+	'mk': {
+		 'category_redirect-fix-double': u'Бот: Исправка на двојни пренасочувања',
+	},
+	'ms': {
+		'category_redirect-fix-double': u'Bot: Memperbetulkan pelencongan berganda',
+	},
+	'mt': {
+		'category_redirect-fix-double': u'Bot: Tranġar ta\' rindirizz doppju',
+	},
+	'mzn': {
+		'category_redirect-fix-double': u'ربوت:عوض هایتن دکشیه‌ئون دِتایی',
+	},
+	'nds': {
+		'category_redirect-fix-double': u'Bot: Dubbelte Wiederleiden rutmakt',
+	},
+	'nl': {
+		'category_redirect-fix-double': u'Robot: dubbele doorverwijzing gecorrigeerd',
+	},
+	'nn': {
+		'category_redirect-fix-double': u'robot: retta dobbel omdirigering',
+	},
+	'no': {
+		'category_redirect-fix-double': u'robot: Retter dobbel omdirigering',
+	},
+	'pdc': {
+		'category_redirect-fix-double': u'Waddefresser: Doppelte Weiderleiding',
+	},
+	'pfl': {
+		'category_redirect-fix-double': u'Bot: E doppelte Waiterlaitung vabessat',
+	},
+	'pl': {
+		'category_redirect-change-category': u'Robot: Usuwa strony z przekierowanej kategorii',
+		'category_redirect-add-template': u'Robot: Dodaje szablon przekierowanej kategorii',
+		'category_redirect-fix-double': u'Robot: Naprawia podwójne przekierowanie',
+		'category_redirect-comment': u'Robot porządkujący przekierowania kategorii',
+		'category_redirect-edit-request-item': u'* %(title)s jest w %(oldcat)s, która jest przekierowaniem do %(newcat)s',
+		'category_redirect-edit-request': u'Następujące zabezpieczone strony wykryto jako wymagające poprawy kategorii: %(itemlist)s',
+	},
+	'pt': {
+		'category_redirect-change-category': u'Bot: movendo páginas de redirecionamentos de categorias',
+		'category_redirect-add-template': u'Bot: adicionando a predefinição de redirecionamento de categoria',
+		'category_redirect-fix-double': u'Robô: Corrigindo redirecionamento duplo',
+		'category_redirect-comment': u'Bot de manutenção de categorias de redirecionamento',
+		'category_redirect-edit-request-item': u'* %(title)s está em %(oldcat)s, que redireciona para %(newcat)s',
+		'category_redirect-edit-request': u'As seguintes páginas protegidas foram detectadas como carecendo de actualizações de ligações de categorias: %(itemlist)s',
+	},
+	'pt-br': {
+		'category_redirect-fix-double': u'Robô: Corrigido redirecionamento duplo',
+	},
+	'ro': {
+		'category_redirect-fix-double': u'Robot: Reparat dubla redirecționare înspre',
+	},
+	'ru': {
+		'category_redirect-fix-double': u'Робот: исправление двойного перенаправления',
+	},
+	'rue': {
+		'category_redirect-fix-double': u'Робот: справив двоїте напрямлїня',
+	},
+	'sl': {
+		'category_redirect-fix-double': u'Bot: Popravljanje dvojnih preusmeritev',
+	},
+	'sr': {
+		'category_redirect-fix-double': u'Бот: исправљање двоструких преусмерења',
+	},
+	'sr-ec': {
+		'category_redirect-fix-double': u'Бот: исправљање двоструких преусмерења',
+	},
+	'sr-el': {
+		'category_redirect-fix-double': u'Bot: ispravljanje dvostrukih preusmerenja',
+	},
+	'sv': {
+		'category_redirect-fix-double': u'Robot: Rättar dubbel omdirigering',
+	},
+	'szl': {
+		'category_redirect-fix-double': u'Robot sprowjo tuplowane przekerowańa',
+	},
+	'th': {
+		'category_redirect-fix-double': u'โรบอต: แก้หน้าเปลี่ยนทางซ้ำซ้อน',
+	},
+	'tr': {
+		'category_redirect-fix-double': u'Bot değişikliği: Yönlendirmeye olan yönlendirme',
+	},
+	'tt': {
+		'category_redirect-fix-double': u'Робот: икеле күчешне дөресләү',
+	},
+	'uk': {
+		'category_redirect-fix-double': u'Робот: виправлення подвійного перенаправлення',
+	},
+	'vi': {
+		'category_redirect-change-category': u'Robot: bỏ trang ra khỏi thể loại đổi hướng',
+		'category_redirect-add-template': u'Robot: thêm bản mẫu đổi hướng thể loại để dễ bảo trì',
+		'category_redirect-fix-double': u'Robot: sửa thể loại đổi hướng kép',
+		'category_redirect-comment': u'Robot theo dõi thể loại đổi hướng',
+		'category_redirect-edit-request-item': u'* %(title)s đang thuộc %(oldcat)s, là thể loại đổi hướng đến %(newcat)s',
+		'category_redirect-edit-request': u'Các trang đã khóa sau cần phải cập nhật liên kết thể loại: %(itemlist)s',
+	},
+	'war': {
+		'category_redirect-fix-double': u'Robot: Gin-ayad in nagduduha nga redirek',
+	},
+	'yi': {
+		'category_redirect-fix-double': u'באט: פארראכטן פארטאפלטע ווייטערפירונג',
+	},
+	'zh': {
+		'category_redirect-change-category': u'机器人：改变已重定向分类中的页面的分类',
+		'category_redirect-add-template': u'机器人: 增加分类重定向模板，用于维护',
+		'category_redirect-fix-double': u'機器人:修正雙重重定向',
+		'category_redirect-comment': u'分类重定向维护机器人',
+		'category_redirect-edit-request': u'下列被保护页面被检测出需要更新 分类链接: %(itemlist)s',
+	},
+	'zh-classical': {
+		'category_redirect-fix-double': u'僕:復修渡口',
+	},
+	'zh-hans': {
+		'category_redirect-fix-double': u'机器人:修正双重重定向',
+	},
+	'zh-yue': {
+		'category_redirect-fix-double': u'機械人：拉直連串跳轉',
+	},
+}
