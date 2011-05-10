b7241fe96fea8f0e0184cda570c236cea1caf2fa
xqt <xqt@users.mediawiki.org>
2011-05-10 09:16:58 +0000

    i18n tw summaries for clean_sandbox
    https://mediawiki.org/wiki/Special:Code/pywikipedia/9234

---
+++ clean_sandbox.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+msg = {
+	'als': {
+		'clean_sandbox-cleaned': u'Bötli: Sandchaschte iigebnet.',
+	},
+	'ar': {
+		'clean_sandbox-cleaned': u'روبوت: هذه الصفحة سيتم تفريغها تلقائيا',
+	},
+	'bar': {
+		'clean_sandbox-cleaned': u'Bot: Spielwiesn gmaht.',
+	},
+	'cs': {
+		'clean_sandbox-cleaned': u'Uhrabání pískoviště',
+	},
+	'da': {
+		'clean_sandbox-cleaned': u'Bot: Nyt sand (fra[[Skabelon:Sandkasse tekst]])',
+	},
+	'de': {
+		'clean_sandbox-cleaned': u'Bot: Setze Spielwiese zurück.',
+	},
+	'en': {
+		'clean_sandbox-cleaned': u'Robot: Automatically cleaned',
+	},
+	'fa': {
+		'clean_sandbox-cleaned': u'ربات: صفحه به طور خودکار تميز شد',
+	},
+	'fi': {
+		'clean_sandbox-cleaned': u'Botti siivosi hiekkalaatikon.',
+	},
+	'he': {
+		'clean_sandbox-cleaned': u'בוט: דף זה ינוקה אוטומטית.',
+	},
+	'id': {
+		'clean_sandbox-cleaned': u'Bot: Tata ulang',
+	},
+	'it': {
+		'clean_sandbox-cleaned': u'Bot: pulitura sandbox',
+	},
+	'ja': {
+		'clean_sandbox-cleaned': u'ロボットによる: 砂場ならし',
+	},
+	'ko': {
+		'clean_sandbox-cleaned': u'로봇: 연습장 비움',
+	},
+	'ksh': {
+		'clean_sandbox-cleaned': u'Bot: allt Zeush fott gedunn.',
+	},
+	'nds': {
+		'clean_sandbox-cleaned': u'Bot: Speelwisch leddig maakt.',
+	},
+	'nl': {
+		'clean_sandbox-cleaned': u'Bot: automatisch voorzien van schoon zand.',
+	},
+	'no': {
+		'clean_sandbox-cleaned': u'bot: Rydder sandkassa.',
+	},
+	'pl': {
+		'clean_sandbox-cleaned': u'Robot czyści brudnopis',
+	},
+	'pt': {
+		'clean_sandbox-cleaned': u'Bot: Limpeza da página de testes',
+	},
+	'qqq': {
+		'clean_sandbox-cleaned': u'Summary message when the bot cleans sandbox',
+	},
+	'ru': {
+		'clean_sandbox-cleaned': u'Бот: очистка песочницы',
+	},
+	'sr': {
+		'clean_sandbox-cleaned': u'Чишћење песка',
+	},
+	'sv': {
+		'clean_sandbox-cleaned': u'Robot krattar sandlådan.',
+	},
+	'th': {
+		'clean_sandbox-cleaned': u'โรบอต: กำลังจะเก็บกวาดหน้านี้โดยอัตโนมัติ',
+	},
+	'zh': {
+		'clean_sandbox-cleaned': u'Bot: 本页被自动清理',
+	},
+}
