04b1e4920aabb43823e879c28dc36d04a8f66b7c
Maarten Dammers <multichill@users.mediawiki.org>
2011-07-09 14:26:55 +0000

    Make all variables named.
    https://mediawiki.org/wiki/Special:Code/pywikipedia/9347

--- commons.py
+++ commons.py
@@ -1,28 +1,28 @@
 # -*- coding: utf-8 -*-
 msg = {
 	'ar': {
-                'commons-file-moved' : u'[[:File:%s|الصورة]] تم نقلها إلى [[:commons:File:%s|كومنز]].',
+                'commons-file-moved' : u'[[:File:%(localfile)s|الصورة]] تم نقلها إلى [[:commons:File:%(commonsfile)s|كومنز]].',
                 'commons-file-now-available' : u'الملف الآن متوفر في ويكيميديا كومنز.',
 	},
         'de': {
                 'commons-file-now-available' : u'Datei ist jetzt auf Wikimedia Commons verfügbar.',
 	},
         'en': {
-                'commons-file-moved' : u'[[:File:%s|File]] moved to [[:commons:File:%s|commons]].',
+                'commons-file-moved' : u'[[:File:%(localfile)s|File]] moved to [[:commons:File:%(commonsfile)s|commons]].',
                 'commons-file-now-available' : u'File is now available on Wikimedia Commons.',
 	},
         'eo': {
                 'commons-file-now-available' : u'Dosiero nun estas havebla en la Wikimedia-Komunejo.',
 	},
         'fa': {
-                'commons-file-moved' : u'[[:پرونده:%s|پرونده]] به [[:commons:File:%s|commons]] منتقل شد.',
+                'commons-file-moved' : u'[[:پرونده:%(localfile)s|پرونده]] به [[:commons:File:%(commonsfile)s|commons]] منتقل شد.',
                 'commons-file-now-available' : u'اینک پرونده در ویکی‌انبار قابل دسترسی است.',
 	},
         'he': {
                 'commons-file-now-available' : u'File is now available on Wikimedia Commons.',
 	},
         'hu': {
-                'commons-file-moved' : u'[[:File:%s|Kép]] átmozgatva a [[:commons:File:%s|Commons]]ba.',
+                'commons-file-moved' : u'[[:File:%(localfile)s|Kép]] átmozgatva a [[:commons:File:%(commonsfile)s|Commons]]ba.',
                 'commons-file-now-available' : u'A fájl most már elérhető a Wikimedia Commonson.',
 	},
         'ia': {
@@ -41,29 +41,29 @@ msg = {
                 'commons-file-now-available' : u'Disse ofbeelding is beschikbaor op Wikimedia Commons.',
 	},
         'nl': {
-                'commons-file-moved' : u'[[:File:%s|Bestand]] is verplaatst naar [[:commons:File:%s|commons]].',
+                'commons-file-moved' : u'[[:File:%(localfile)s|Bestand]] is verplaatst naar [[:commons:File:%(commonsfile)s|commons]].',
                 'commons-file-now-available' : u'Dit bestand staat nu op [[w:nl:Wikimedia Commons|Wikimedia Commons]].',
 	},
         'pl': {
-                'commons-file-moved' : u'[[:File:%s|Plik]] przeniesiona do [[:commons:File:%s|commons]].',
+                'commons-file-moved' : u'[[:File:%(localfile)s|Plik]] przeniesiona do [[:commons:File:%(commonsfile)s|commons]].',
                 'commons-file-now-available' : u'Plik jest teraz dostępny na Wikimedia Commons.',
 	},
         'pt': {
                 'commons-file-now-available' : u'Arquivo está agora na Wikimedia Commons.',
 	},
 	'qqq': {
-		'commons-file-now-available' : u'Edit summary when the bot has moved a file to Commons and adds the NowCommons template.',
-                'commons-file-moved' : u'Edit summary when the bot replaces usage of an image available under a different name.',
+		'commons-file-now-available' : u'Edit summary when the bot has moved a file to Commons and adds the NowCommons template. %(localfile)s contains the local filename and %(commonsfile)s the filename at Commons.',
+                'commons-file-moved' : u'Edit summary when the bot replaces usage of an image available under a different name. %(localfile)s contains the local filename and %(commonsfile)s the filename at Commons.',
 	},
         'ru': {
-                'commons-file-moved' : u'[[:File:%s|Файл]] перемещён на [[:commons:File:%s|Викисклад]].',
+                'commons-file-moved' : u'[[:File:%(localfile)s|Файл]] перемещён на [[:commons:File:%(commonsfile)s|Викисклад]].',
                 'commons-file-now-available' : u'Файл теперь доступен на Викискладе.',
 	},
         'sr': {
                 'commons-file-now-available' : u'Слика је сада доступна и на Викимедија Остави.',
 	},
         'zh': {
-                'commons-file-moved' : u'[[:File:%s|本檔案]]已移至[[:commons:File:%s|維基共享資源]]',
+                'commons-file-moved' : u'[[:File:%(localfile)s|本檔案]]已移至[[:commons:File:%(commonsfile)s|維基共享資源]]',
                 'commons-file-now-available' : u'檔案已存在於維基共享資源。',
 	},
 }
