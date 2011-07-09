d5494af7894f18ca0b9e59682c3cc07cedf17fdc
Maarten Dammers <multichill@users.mediawiki.org>
2011-07-09 14:35:24 +0000

    Link to the file at Commons.
    https://mediawiki.org/wiki/Special:Code/pywikipedia/9348

--- commons.py
+++ commons.py
@@ -5,11 +5,11 @@ msg = {
                 'commons-file-now-available' : u'الملف الآن متوفر في ويكيميديا كومنز.',
 	},
         'de': {
-                'commons-file-now-available' : u'Datei ist jetzt auf Wikimedia Commons verfügbar.',
+                'commons-file-now-available' : u'Datei ist jetzt auf [[:commons:File:%(commonsfile)s|Wikimedia Commons]] verfügbar.',
 	},
         'en': {
                 'commons-file-moved' : u'[[:File:%(localfile)s|File]] moved to [[:commons:File:%(commonsfile)s|commons]].',
-                'commons-file-now-available' : u'File is now available on Wikimedia Commons.',
+                'commons-file-now-available' : u'File is now available on [[:commons:File:%(commonsfile)s|Wikimedia Commons]].',
 	},
         'eo': {
                 'commons-file-now-available' : u'Dosiero nun estas havebla en la Wikimedia-Komunejo.',
@@ -19,37 +19,37 @@ msg = {
                 'commons-file-now-available' : u'اینک پرونده در ویکی‌انبار قابل دسترسی است.',
 	},
         'he': {
-                'commons-file-now-available' : u'File is now available on Wikimedia Commons.',
+                'commons-file-now-available' : u'File is now available on [[:commons:File:%(commonsfile)s|Wikimedia Commons]].',
 	},
         'hu': {
                 'commons-file-moved' : u'[[:File:%(localfile)s|Kép]] átmozgatva a [[:commons:File:%(commonsfile)s|Commons]]ba.',
                 'commons-file-now-available' : u'A fájl most már elérhető a Wikimedia Commonson.',
 	},
         'ia': {
-                'commons-file-now-available' : u'Le file es ora disponibile in Wikimedia Commons.',
+                'commons-file-now-available' : u'Le file es ora disponibile in [[:commons:File:%(commonsfile)s|Wikimedia Commons]].',
 	},
         'it': {
-                'commons-file-now-available' : u'L\'immagine è adesso disponibile su Wikimedia Commons.',
+                'commons-file-now-available' : u'L\'immagine è adesso disponibile su [[:commons:File:%(commonsfile)s|Wikimedia Commons]].',
 	},
         'kk': {
                 'commons-file-now-available' : u'Файлды енді Wikimedia Ортаққорынан қатынауға болады.',
 	},
         'lt': {
-                'commons-file-now-available' : u'Failas įkeltas į Wikimedia Commons projektą.',
+                'commons-file-now-available' : u'Failas įkeltas į [[:commons:File:%(commonsfile)s|Wikimedia Commons projektą]].',
 	},
         'nds-nl': {
-                'commons-file-now-available' : u'Disse ofbeelding is beschikbaor op Wikimedia Commons.',
+                'commons-file-now-available' : u'Disse ofbeelding is beschikbaor op [[:commons:File:%(commonsfile)s|Wikimedia Commons]].',
 	},
         'nl': {
                 'commons-file-moved' : u'[[:File:%(localfile)s|Bestand]] is verplaatst naar [[:commons:File:%(commonsfile)s|commons]].',
-                'commons-file-now-available' : u'Dit bestand staat nu op [[w:nl:Wikimedia Commons|Wikimedia Commons]].',
+                'commons-file-now-available' : u'Dit bestand staat nu op [[:commons:File:%(commonsfile)s|Wikimedia Commons]].',
 	},
         'pl': {
                 'commons-file-moved' : u'[[:File:%(localfile)s|Plik]] przeniesiona do [[:commons:File:%(commonsfile)s|commons]].',
-                'commons-file-now-available' : u'Plik jest teraz dostępny na Wikimedia Commons.',
+                'commons-file-now-available' : u'Plik jest teraz dostępny na [[:commons:File:%(commonsfile)s|Wikimedia Commons]].',
 	},
         'pt': {
-                'commons-file-now-available' : u'Arquivo está agora na Wikimedia Commons.',
+                'commons-file-now-available' : u'Arquivo está agora na [[:commons:File:%(commonsfile)s|Wikimedia Commons]].',
 	},
 	'qqq': {
 		'commons-file-now-available' : u'Edit summary when the bot has moved a file to Commons and adds the NowCommons template. %(localfile)s contains the local filename and %(commonsfile)s the filename at Commons.',
