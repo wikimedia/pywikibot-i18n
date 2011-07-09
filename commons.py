8fc0b08d3b6579a6bc7ca80a8b5535ba5d119e17
Maarten Dammers <multichill@users.mediawiki.org>
2011-07-09 12:11:54 +0000

    Start with moving Commons messages to Translatewiki. Still have to import the existing messages.
    https://mediawiki.org/wiki/Special:Code/pywikipedia/9343

---
+++ commons.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+msg = {
+	'en': {
+                'commons-file-moved' : u'[[:File:%s|File]] moved to [[:commons:File:%s|commons]].',
+                'commons-file-now-available' : u'File is now available on Wikimedia Commons.',
+                'commons-nowcommons-template' : 'en': u'{{subst:ncd|%s}}',
+	},
+	'qqq': {
+		'commons-file-now-available': u'Edit summary when the bot has moved a file to Commons and adds the NowCommons template.',
+                'commons-file-moved': u'Edit summary when the bot replaces usage of an image available under a different name.',
+                'commons-nowcommons-template' : 'en': u'The template to be added by the bot when a file is available at Commons. %s is the filename at Commons.',
+	},
+}
