d4adfa6f52d0d4d295cf12caea807d9c7b06d3ad
xqt <xqt@users.mediawiki.org>
2011-07-10 10:28:44 +0000

    i18n comments for welcome.py
    https://mediawiki.org/wiki/Special:Code/pywikipedia/9353

---
+++ welcome.py
@@ -0,0 +1,111 @@
+﻿msg = {
+        'en': {
+                'welcome-bad_username': u'Bot: Adding a username that needs to be checked',
+                'welcome-updating': u'Bot: Updating log',
+                'welcome-welcome': u'Welcome!',
+        },
+        'zh': {
+                'welcome-bad_username': u'Bot: 回報不適當的用戶名稱',
+                'welcome-updating': u'Bot: 更新日志',
+                'welcome-welcome': u'欢迎！',
+        },
+        'am': {
+                'welcome-bad_username': u'Bot: Adding a username that needs to be checked',
+                'welcome-updating': u'Bot: Updating log',
+                'welcome-welcome': u'Welcome to Amharic Wikipedia!',
+        },
+        'it': {
+                'welcome-bad_username': u'Bot: Aggiunto utente da controllare',
+                'welcome-updating': u'Bot: Aggiorno il log',
+                'welcome-welcome': u'Benvenuto!',
+        },
+        'vo': {
+                'welcome-welcome': u'Benokömö!',
+        },
+        'ar': {
+                'welcome-bad_username': u'Bot: إضافة اسم مستخدم يحتاج للفحص',
+                'welcome-updating': u'Bot: تحديث السجل',
+                'welcome-welcome': u'مرحبا!',
+        },
+        'ga': {
+                'welcome-updating': u'Bot: Log a thabhairt suas chun dáta',
+                'welcome-welcome': u'Fáilte!',
+        },
+        'id': {
+                'welcome-welcome': u'Selamat datang',
+        },
+        'ru': {
+                'welcome-bad_username': u'Bot: Добавлено подозрительное имя участника',
+                'welcome-updating': u'Bot: Обновление',
+                'welcome-welcome': u'Добро пожаловать!',
+        },
+        'nl': {
+                'welcome-bad_username': u'Bot: Te controleren gebruikersnaam toegevoegd',
+                'welcome-updating': u'Bot: Logboek bijwerken',
+                'welcome-welcome': u'Welkom!',
+        },
+        'pt': {
+                'welcome-welcome': u'Bem vindo!',
+        },
+        'no': {
+                'welcome-bad_username': u'Bot: Legger til et brukernavn som m? sjekkes',
+                'welcome-updating': u'Bot: Oppdaterer logg',
+                'welcome-welcome': u'Velkommen!',
+        },
+        'fr': {
+                'welcome-updating': u'Bot: Mise a jour du journal',
+                'welcome-welcome': u'Bienvenue sur Wikipedia !',
+        },
+        'zh-yue': {
+                'welcome-updating': u'Bot: 更新日誌',
+                'welcome-welcome': u'歡迎',
+        },
+        'bn': {
+                'welcome-welcome': u'Welcome!',
+        },
+        'de': {
+                'welcome-bad_username': u'Bot: Ergänze zu überprüfenden Benutzernamen',
+                'welcome-updating': u'Bot: Aktualisiere Logdatei',
+                'welcome-welcome': u'Herzlich willkommen!',
+        },
+        'da': {
+                'welcome-bad_username': u'Bot: Adding a username that needs to be checked',
+                'welcome-updating': u'Bot: Updating log',
+                'welcome-welcome': u'Velkommen',
+        },
+        'fa': {
+                'welcome-bad_username': u'Bot: افزودن حساب کاربری نیازمند بررسی',
+                'welcome-updating': u'Bot: به روز رسانی سیاهه',
+                'welcome-welcome': u'خوش آمدید!',
+        },
+        'qqq': {
+                'welcome-bad_username': u'Bot: edit summary for reporting a possibly bad username',
+                'welcome-updating': u'Bot: edit summary for updating the welcome log',
+                'welcome-welcome': u'Edit summary for the welcome message',
+        },
+        'ja': {
+                'welcome-bad_username': u'Bot: 不適切な利用者名の報告',
+                'welcome-updating': u'Bot: 更新記録',
+                'welcome-welcome': u'ウィキペディア日本語版へようこそ！',
+        },
+        'he': {
+                'welcome-welcome': u'ברוך הבא!',
+        },
+        'ka': {
+                'welcome-welcome': u'კეთილი იყოს თქვენი მობრძანება!',
+        },
+        'sr': {
+                'welcome-updating': u'Bot: Освежавање записа',
+                'welcome-welcome': u'Добродошли!',
+        },
+        'sq': {
+                'welcome-bad_username': u'Bot: Added username to be checked',
+                'welcome-updating': u'Bot: Rifreskoj log',
+                'welcome-welcome': u'Tung',
+        },
+        'pdc': {
+                'welcome-bad_username': u'Bot: Yuusernaame zum iwwerpriefe',
+                'welcome-updating': u'Bot: Logfeil ennere',
+                'welcome-welcome': u'Wilkum!',
+        },
+}
