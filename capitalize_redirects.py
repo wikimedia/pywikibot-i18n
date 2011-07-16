b1cd8256e396e959b435b1cbb45fe7e359f7297f
xqt <xqt@users.mediawiki.org>
2011-07-16 10:23:53 +0000

    new i18n tw dictionary
    https://mediawiki.org/wiki/Special:Code/pywikipedia/9389

---
+++ capitalize_redirects.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+msg = {
+        'ar': {
+                'capitalize_redirects-create-redirect': u'روبوت: إنشاء تحويلة إلى [[%s]]',
+        },
+        'cs': {
+                'capitalize_redirects-create-redirect': u'Robot vytvořil přesměrování na [[%s]]',
+        },
+        'de': {
+                'capitalize_redirects-create-redirect': u'Bot: Weiterleitung angelegt auf [[%s]]',
+        },
+        'en': {
+                'capitalize_redirects-create-redirect': u'Robot: Create redirect to [[%s]]',
+        },
+        'fr': {
+                'capitalize_redirects-create-redirect': u'robot: Créer redirection à [[%s]]',
+        },
+        'he': {
+                'capitalize_redirects-create-redirect': u'בוט: יוצר הפניה לדף [[%s]]',
+        },
+        'ja': {
+                'capitalize_redirects-create-redirect': u'ロボットによる: リダイレクト作成 [[%s]]',
+        },
+        'ksh': {
+                'capitalize_redirects-create-redirect': u'Bot: oemleidung aanjelaat op [[%s]]',
+        },
+        'nl': {
+                'capitalize_redirects-create-redirect': u'Bot: doorverwijzing gemaakt naar [[%s]]',
+        },
+        'pt': {
+                'capitalize_redirects-create-redirect': u'Bot: Criando redirecionamento para [[%s]]',
+        },
+        'ru': {
+                'capitalize_redirects-create-redirect': u'Бот: Создано перенаправление на [[%s]]',
+        },
+        'sv': {
+                'capitalize_redirects-create-redirect': u'Bot: Omdirigerar till [[%s]]',
+        },
+        'uk': {
+                'capitalize_redirects-create-redirect': u'Бот: Створено перенаправлення на [[%s]]',
+        },
+        'zh': {
+                'capitalize_redirects-create-redirect': u'機器人: 建立重定向至[[%s]]',
+        },
+}
