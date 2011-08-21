2dfe7c21c2746a966675f6a1063c5a75de2548b2
xqt <xqt@users.mediawiki.org>
2011-08-21 15:13:45 +0000

    new i18n tw dictionary with plural support
    https://mediawiki.org/wiki/Special:Code/pywikipedia/9450

---
+++ template.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+msg = {
+        'ar': {
+                'template-changing': u'روبوت: تغيير {{PLURAL:%(num)d|القالب|القوالب}}: %(list)s',
+                'template-removing': u'روبوت: إزالة {{PLURAL:%(num)d|القالب|القوالب}}: %(list)s',
+                'template-substituting': u'روبوت: نسخ {{PLURAL:%(num)d|القالب|القوالب}}: %(list)s',
+        },
+        'da': {
+                'template-changing': u'Bot: Erstatter {{PLURAL:%(num)d|skabelon|skabeloner}}: %(list)s',
+                'template-removing': u'Bot: Fjerner {{PLURAL:%(num)d|skabelon|skabeloner}}: %(list)s',
+                'template-substituting': u'Bot: Substituerer {{PLURAL:%(num)d|skabelon|skabeloner}}: %(list)s',
+        },
+        'de': {
+                'template-changing': u'Bot: Ändere {{PLURAL:%(num)d|Vorlage|Vorlagen}}: %(list)s',
+                'template-removing': u'Bot: Entferne {{PLURAL:%(num)d|Vorlage|Vorlagen}}: %(list)s',
+                'template-substituting': u'Bot: Umgehe {{PLURAL:%(num)d|Vorlage|Vorlagen}}: %(list)s',
+        },
+        'en': {
+                'template-changing': u'Bot: Changing {{PLURAL:%(num)d|template|templates}}: %(list)s',
+                'template-removing': u'Robot: Removing {{PLURAL:%(num)d|template|templates}}: %(list)s',
+                'template-substituting': u'Robot: Substituting {{PLURAL:%(num)d|template|templates}}: %(list)s',
+        },
+        'es': {
+                'template-changing': u'Bot: Cambiada la {{PLURAL:%(num)d|la plantilla|las plantillas}}: %(list)s',
+                'template-removing': u'Robot: Retirando {{PLURAL:%(num)d|la plantilla|las plantillas}}: %(list)s',
+                'template-substituting': u'Robot: Sustituyendo la {{PLURAL:%(num)d|la plantilla|las plantillas}}: %(list)s',
+        },
+        'fa': {
+                'template-changing': u'ربات:تغییر {{PLURAL:%(num)d|الگو|الگوها}}: %(list)s',
+                'template-removing': u'ربات:حذف {{PLURAL:%(num)d|الگو|الگوها}}: %(list)s',
+                'template-substituting': u'ربات: جایگزینی {{PLURAL:%(num)d|الگو|الگوها}}: %(list)s',
+        },
+        'fi': {
+                'template-changing': u'Botti korvasi {{PLURAL:%(num)d|mallineen|mallineet}}: %(list)s',
+                'template-removing': u'Botti poisti {{PLURAL:%(num)d|mallineen|mallineet}}',
+                'template-substituting': u'Botti substasi {{PLURAL:%(num)d|mallineen|mallineet}}',
+        },
+        'fr': {
+                'template-changing': u'Robot : {{PLURAL:%(num)d|Change modèle|Modifie modèles}}: %(list)s',
+                'template-removing': u'Robot : Enlève {{PLURAL:%(num)d|le modèle|modèles}}: %(list)s',
+                'template-substituting': u'Robot : Remplace {{PLURAL:%(num)d|modèle|modèles}}: %(list)s',
+        },
+        'he': {
+                'template-changing': u'בוט: משנה {{PLURAL:%(num)d|תבנית|תבניות}}: %(list)s',
+                'template-removing': u'בוט: מסיר {{PLURAL:%(num)d|תבנית|תבניות}}: %(list)s',
+                'template-substituting': u'בוט: מכליל {{PLURAL:%(num)d|תבנית|תבניות}} בקוד הדף: %(list)s',
+        },
+        'hu': {
+                'template-changing': u'Bot: Sablon csere: %(list)s',
+                'template-removing': u'Robot: Sablon eltávolítása: %(list)s',
+        },
+        'ia': {
+                'template-changing': u'Robot: Modification del template: %(list)s',
+                'template-removing': u'Robot: Elimination del template: %(list)s',
+        },
+        'ja': {
+                'template-changing': u'ロボットによるテンプレートの張り替え : %(list)s',
+                'template-removing': u'ロボットによるテンプレートの除去 : %(list)s',
+                'template-substituting': u'ロボットによるテンプレートの置換 : %(list)s',
+        },
+        'kk': {
+                'template-changing': u'Бот: Мына {{PLURAL:%(num)d|үлгі|үлгілер}} өзгертілді: %(list)s',
+                'template-removing': u'Бот: Мына {{PLURAL:%(num)d|үлгі|үлгілер}} аластатылды: %(list)s',
+                'template-substituting': u'Бот: Мына {{PLURAL:%(num)d|үлгі|үлгілер}} бәделдірленді: %(list)s',
+        },
+        'lt': {
+                'template-changing': u'robotas: {{PLURAL:%(num)d|Keičiamas šablonas|Keičiami šablonai}}: %(list)s',
+                'template-removing': u'robotas: {{PLURAL;%(num)d|Šalinamas šablonas|Šalinami šablonai}}: %(list)s',
+        },
+        'nds': {
+                'template-changing': u'Bot: {{PLURAL:%(num)d|Vörlaag|Vörlagen}} utwesselt: %(list)s',
+                'template-removing': u'Bot: {{PLURAL:%(num)d|Vörlaag|Vörlagen}} rut: %(list)s',
+                'template-substituting': u'Bot: {{PLURAL:%(num)d|Vörlaag|Vörlagen}} in Text övernahmen: %(list)s',
+        },
+        'nl': {
+                'template-changing': u'Bot: vervangen {{PLURAL:%(num)d|sjabloon|sjablonen}}: %(list)s',
+                'template-removing': u'Bot: verwijderen {{PLURAL:%(num)d|sjabloon|sjablonen}}: %(list)s',
+                'template-substituting': u'Bot: substitueren {{PLURAL:%(num)d|sjabloon|sjablonen}}: %(list)s',
+        },
+        'no': {
+                'template-changing': u'bot: Endrer {{PLURAL:%(num)d|mal|maler}}: %(list)s',
+                'template-removing': u'bot: Fjerner {{PLURAL:%(num)d|mal|maler}}: %(list)s',
+                'template-substituting': u'bot: Erstatter {{PLURAL:%(num)d|mal|maler}}: %(list)s',
+        },
+        'pl': {
+                'template-changing': u'Robot zmienia {{PLURAL:%(num)d|szablon|szablony}}: %(list)s',
+                'template-removing': u'Robot usuwa {{PLURAL:%(num)d|szablon|szablony}}: %(list)s',
+                'template-substituting': u'Robot podmienia {{PLURAL:%(num)d|szablon|szablony}}: %(list)s',
+        },
+        'pt': {
+                'template-changing': u'Bot: Alterando predefinição: %(list)s',
+                'template-removing': u'Bot: Removendo predefinição: %(list)s',
+                'template-substituting': u'Bot: Substituindo predefinição: %(list)s',
+        },
+        'ru': {
+                'template-changing': u'Бот: замена {{PLURAL:%(num)d|шаблона|шаблонов}}: %(list)s',
+                'template-removing': u'Бот: удаление {{PLURAL:%(num)d|шаблона|шаблонов}}: %(list)s',
+                'template-substituting': u'Бот: подстановка {{PLURAL:%(num)d|шаблона|шаблонов}}: %(list)s',
+        },
+        'sr': {
+                'template-changing': u'Бот: Измена шаблона: %(list)s',
+                'template-removing': u'Бот: Уклањање шаблона: %(list)s',
+        },
+        'uk': {
+                'template-changing': u'Бот: заміна {{PLURAL:%(num)d|шаблону|шаблонів}}: %(list)s',
+                'template-removing': u'Робот: видалення {{PLURAL:%(num)d|шаблону|шаблонів}}: %(list)s',
+                'template-substituting': u'Робот: підстановка {{PLURAL:%(num)d|шаблону|шаблонів}}: %(list)s',
+        },
+        'zh': {
+                'template-changing': u'機器人: 更改模板 %(list)s',
+                'template-removing': u'機器人: 移除模板 %(list)s',
+                'template-substituting': u'機器人: 更換模板 %(list)s',
+        },
+}
