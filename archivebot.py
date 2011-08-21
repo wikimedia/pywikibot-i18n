cd184edd7897df3eb8834eaccf8f4b7c76c13ff6
xqt <xqt@users.mediawiki.org>
2011-08-21 09:41:46 +0000

    new i18n tw dictionary
    https://mediawiki.org/wiki/Special:Code/pywikipedia/9446

---
+++ archivebot.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+msg = {
+        'ar': {
+            'archivebot-archive-full': u'(الأرشيف ممتلئ)',
+            'archivebot-archiveheader': u'{{أرشيف نقاش}}',
+            'archivebot-page-summary': u'أرشفة %(count)d قسم(أقسام) (%(why)s) إلى %(archives)s.',
+            'archivebot-archive-summary': u'أرشفة %(count)d قسم(أقسام) من [[%(from)s]].',
+            'archivebot-older-than': u'أقدم من',
+            },
+        'en': {
+            'archivebot-archive-full': u'(ARCHIVE FULL)',
+            'archivebot-archiveheader': u'{{talkarchive}}',
+            'archivebot-page-summary': u'Archiving %(count)d thread(s) (%(why)s) to %(archives)s.',
+            'archivebot-archive-summary': u'Archiving %(count)d thread(s) from [[%(from)s]].',
+            'archivebot-older-than': u'older than',
+            },
+        'fi': {
+            'archivebot-archive-full' : u'(ARKISTO TÄYSI)',
+            'archivebot-archiveheader': u'{{arkisto}}',
+            'archivebot-page-summary': u'Arkistoidaan %(count)d keskustelua (%(why)s) %(archives)s arkistoon.',
+            'archivebot-archive-summary': u'Arkistoidaan %(count)d keskustelua sivulta [[%(from)s]].',
+            'archivebot-older-than': u'vanhempi kuin',
+            },
+        'hu': {
+            'archivebot-archive-full': u'(ARCHÍVUM BETELT)',
+            'archivebot-archiveheader': u'{{archív lap}}',
+            'archivebot-page-summary': u'%(count)d szakasz archiválása (%(why)s) a(z) %(archives)s lapra.',
+            'archivebot-archive-summary': u'%(count)d szakasz archiválása a(z) [[%(from)s]] lapról.',
+            'archivebot-older-than': u'régebbi, mint',
+            },
+
+        'ksh': {
+            'archivebot-archive-full': u'(DAT ASCHIHV ES VOLL)',
+            'archivebot-archiveheader': u'{{Sigg weed aschiveet}}',
+            'archivebot-page-summary': u'* %(count)d Schtöe) (%(why)s) en et Aschif %(archives)s jedonn.',
+            'archivebot-archive-summary': u'* %(count)d Schtö) vun [[%(from)s]] noh heh en et Aschihf jedonn.',
+            'archivebot-older-than': u'äer wi',
+             },
+        'nn': {
+            'archivebot-archive-full': u'(ARKIV FULLT)',
+            'archivebot-archiveheader': u'{{arkiv}}',
+            'archivebot-page-summary': u'Arkiverer %(count)d trådar (%(why)s) til %(archives)s.',
+            'archivebot-archive-summary': u'Arkiverer %(count)d trådar frå [[%(from)s]].',
+            'archivebot-older-than': u'eldre enn',
+            },
+        'no': {
+            'archivebot-archive-full': u'(ARKIV FULLT)',
+            'archivebot-archiveheader': u'{{arkiv}}',
+            'archivebot-page-summary': u'Arkiverer %(count)d tråder (%(why)s) til %(archives)s.',
+            'archivebot-archive-summary': u'Arkiverer %(count)d tråder fra [[%(from)s]].',
+            'archivebot-older-than': u'eldre enn',
+            },
+        'pl': {
+            'archivebot-archive-full': u'(ARCHIWUM PEŁNE)',
+            'archivebot-archiveheader': u'{{archiwum}}',
+            'archivebot-page-summary': u'Archiwizacja %(count)d wątków (%(why)s) do %(archives)s.',
+            'archivebot-archive-summary': u'Archiwizacja %(count)d wątków z [[%(from)s]].',
+            'archivebot-older-than': u'starsze niż',
+            },
+        'pt': {
+            'archivebot-archive-full': u'(ARQUIVO COMPLETO)',
+            'archivebot-archiveheader': u'{{talkarchive}}',
+            'archivebot-page-summary': u'Arquivando %(count)d thread(s) (%(why)s) to %(archives)s.',
+            'archivebot-archive-summary': u'Archiving %(count)d thread(s) from [[%(from)s]].',
+            'archivebot-older-than': u'older than',
+            },
+        'qqq': {
+            'archivebot-archiveheader': u'Template that will be put on new archive pages as the header.',
+            'archivebot-page-summary': u'Summary message when the bot removes threads from a talk page for archiving.',
+            'archivebot-archive-summary': u'Summary message when the bot adds threads tho the archive.',
+            },
+        'sv': {
+            'archivebot-archive-full': u'(ARKIV FULLT)',
+            'archivebot-archiveheader': u'{{arkiv}}',
+            'archivebot-page-summary': u'Arkiverar %(count)d trådar (%(why)s) till %(archives)s.',
+            'archivebot-archive-summary': u'Arkiverar %(count)d trådar från [[%(from)s]].',
+            'archivebot-older-than': u'äldre än',
+            },
+}
