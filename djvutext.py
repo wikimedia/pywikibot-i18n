138a8cab12065a75e62de0fb5a8806dd60169bd5
xqt <xqt@users.mediawiki.org>
2011-09-13 15:55:26 +0000

    i18n translations for djvutext.py
    https://mediawiki.org/wiki/Special:Code/pywikipedia/9523

---
+++ djvutext.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+msg = {
+	'ar': {
+		'creating': u'روبوت: إنشاء صفحة بنص مستخرج من DjVu',
+	},
+	'en': {
+		'creating': u'Robot: Creating page with text extracted from DjVu',
+	},
+	'fa': {
+		'creating': u'ربات: ایجاد صفحه با متنی که از دژاوو استخراج شد',
+	},
+	'fr': {
+		'creating': u'Bot: Creating page with texte extracted from DjVu',
+	},
+	'nl': {
+		'creating': u'Bot: pagina aangemaakt met tekst geëxtraheerd uit DjVu-bestand',
+	},
+	'pt': {
+		'creating': u'Bot: criando página com texto extraído do DjVu',
+	},
+}
+
