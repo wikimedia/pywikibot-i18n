d23799e7f191a9ea51a1b44ecc684644368b7f5d
xqt <xqt@users.mediawiki.org>
2011-10-03 11:10:36 +0000

    i18n for movepages
    https://mediawiki.org/wiki/Special:Code/pywikipedia/9570

---
+++ movepages.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+msg = {
+	'ar': {
+		'movepages-moving': u'روبوت: نقل الصفحة',
+	},
+	'cs': {
+		'movepages-moving': u'Robot přesunul stránku',
+	},
+	'de': {
+		'movepages-moving': u'Bot: Seite %(from)s nach %(to)s verschoben',
+	},
+	'el': {
+		'movepages-moving': u'Μετακίνηση σελίδων με bot',
+	},
+	'en': {
+		'movepages-moving': u'Robot: Moved page %(from)s to %(to)s',
+	},
+	'fa': {
+		'movepages-moving': u'ربات: صفحه منتقل شد',
+	},
+	'fi': {
+		'movepages-moving': u'Botti siirsi sivun',
+	},
+	'fr': {
+		'movepages-moving': u'Robot: Page renommée',
+	},
+	'frr': {
+		'movepages-moving':u'Bot: Sidj fersköwen',
+	},
+	'ja': {
+		'movepages-moving': u'ロボットによる: ページの移動',
+	},
+	'he': {
+		'movepages-moving': u'בוט: מעביר דף',
+	},
+	'nl': {
+		'movepages-moving': u'Robot: paginanaam gewijzigd',
+	},
+	'nn': {
+		'movepages-moving': u'robot: flytta sida',
+	},
+	'pl': {
+		'movepages-moving': u'Przeniesienie artykułu przez robota',
+	},
+	'pt': {
+		'movepages-moving': u'Robô: Página movida',
+	},
+	'ru': {
+		'movepages-moving': u'Переименование страницы при помощи робота',
+	},
+	'uk': {
+		'movepages-moving': u'Робот:перейменування сторінки',
+	},
+	'zh': {
+		'movepages-moving': u'機器人:移動頁面',
+	},
+}
