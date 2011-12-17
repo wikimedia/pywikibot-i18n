6eb82382a00bf7be06b2b2b6ab0877f4c4e05b23
xqt <xqt@users.mediawiki.org>
2011-12-17 14:01:16 +0000

    new message file
    https://mediawiki.org/wiki/Special:Code/pywikipedia/9808

---
+++ fixing_redirects.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+msg = {
+        'ar': {
+                'fixing_redirects-fixing': u'بوت: إصلاح التحويلات',
+        },
+        'cs': {
+                'fixing_redirects-fixing': u'Robot opravil přesměrování',
+        },
+        'en': {
+                'fixing_redirects-fixing': u'Bot: Fixing redirects',
+        },
+        'fa': {
+                'fixing_redirects-fixing': u'ربات:تصحیح تغییرمسیرها',
+        },
+        'he': {
+                'fixing_redirects-fixing': u'בוט: מתקן הפניות',
+        },
+        'is': {
+                'fixing_redirects-fixing': u'Robot: Laga tilvísanir',
+        },
+        'ja': {
+                'fixing_redirects-fixing': u'ロボットによる:リダイレクト回避',
+        },
+        'nn': {
+                'fixing_redirects-fixing': u'robot: retta omdirigeringar',
+        },
+        'no': {
+                'fixing_redirects-fixing': u'Robot: Retter omdirigeringer',
+        },
+        'pl': {
+                'fixing_redirects-fixing': u'Bot: naprawa przekierowań',
+        },
+        'pt': {
+                'fixing_redirects-fixing': u'Bot: Arrumando redirects',
+        },
+        'sv': {
+                'fixing_redirects-fixing': u'Bot: Rättar omdirigeringar',
+        },
+        'vi': {
+                'fixing_redirects-fixing': u'Robot: Sửa đổi hướng',
+        },
+        'zh': {
+                'fixing_redirects-fixing': u'機器人: 修復重定向',
+        },
+}
