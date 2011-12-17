4036ff0031e00bac5f5e305d6e06c7fae8ffb073
xqt <xqt@users.mediawiki.org>
2011-12-17 13:48:25 +0000

    fix msg name
    https://mediawiki.org/wiki/Special:Code/pywikipedia/9805

--- djvutext.py
+++ djvutext.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 msg = {
 	'ar': {
-		'creating': u'روبوت: إنشاء صفحة بنص مستخرج من DjVu',
+		'djvutext-creating': u'روبوت: إنشاء صفحة بنص مستخرج من DjVu',
 	},
 	'en': {
-		'creating': u'Robot: Creating page with text extracted from DjVu',
+		'djvutext-creating': u'Robot: Creating page with text extracted from DjVu',
 	},
 	'fa': {
-		'creating': u'ربات: ایجاد صفحه با متنی که از دژاوو استخراج شد',
+		'djvutext-creating': u'ربات: ایجاد صفحه با متنی که از دژاوو استخراج شد',
 	},
 	'fr': {
-		'creating': u'Bot: Creating page with texte extracted from DjVu',
+		'djvutext-creating': u'Bot: Creating page with texte extracted from DjVu',
 	},
 	'nl': {
-		'creating': u'Bot: pagina aangemaakt met tekst geëxtraheerd uit DjVu-bestand',
+		'djvutext-creating': u'Bot: pagina aangemaakt met tekst geëxtraheerd uit DjVu-bestand',
 	},
 	'pt': {
-		'creating': u'Bot: criando página com texto extraído do DjVu',
+		'djvutext-creating': u'Bot: criando página com texto extraído do DjVu',
 	},
 }
 
