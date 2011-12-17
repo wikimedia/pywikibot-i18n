e49d42c6e9f14b6b71d6bb1bd6341d57340b3b74
xqt <xqt@users.mediawiki.org>
2011-12-17 15:25:04 +0000

    new message file
    https://mediawiki.org/wiki/Special:Code/pywikipedia/9810

---
+++ unlink.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+msg = {
+        'ar': {
+                'unlink-unlinking': u'روبوت: إزالة وصلات "%s"',
+        },
+        'de': {
+                'unlink-unlinking': u'Bot: Entlinke "%s"',
+        },
+        'en': {
+                'unlink-unlinking': u'Robot: Unlinking "%s"',
+        },
+        'fa': {
+                'unlink-unlinking': u'ربات: حذف پیوند %s',
+        },
+        'fi': {
+                'unlink-unlinking': u'Botti poisti linkin sivulle "%s"',
+        },
+        'fr': {
+                'unlink-unlinking': u'Robot: Retire le lien "%s"',
+        },
+        'he': {
+                'unlink-unlinking': u'בוט: מסיר קישורים לדף "%s"',
+        },
+        'it': {
+                'unlink-unlinking': u'Bot: eliminazione wikilink diretti a "%s"',
+        },
+        'nl': {
+                'unlink-unlinking': u'Bot: verwijzing naar "%s" verwijderd',
+        },
+        'nn': {
+                'unlink-unlinking': u'robot: fjerna lenkje til "%s"',
+        },
+        'no': {
+                'unlink-unlinking': u'robot: fjerner lenke til "%s"',
+        },
+        'pl': {
+                'unlink-unlinking': u'Robot odlinkowuje "%s"',
+        },
+        'pt': {
+                'unlink-unlinking': u'Bot: Retirando link para "%s"',
+        },
+}
