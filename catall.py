a2823eff28e854fd0a92b3b00087966743ffa2ff
xqt <xqt@users.mediawiki.org>
2011-12-17 13:52:41 +0000

    neu message file
    https://mediawiki.org/wiki/Special:Code/pywikipedia/9806

---
+++ catall.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+msg = {
+        'ar': {
+                'catall-changing': u'بوت: تغيير التصنيفات',
+        },
+        'de': {
+                'catall-changing': u'Bot: Wechsele Kategorien',
+        },
+        'en': {
+                'catall-changing': u'Robot: Changing categories',
+        },
+        'fa': {
+                'catall-changing': u'ربات: تغییر رده‌ها',
+        },
+        'fr': {
+                'catall-changing': u'Bot: Change categories',
+        },
+        'he': {
+                'catall-changing': u'בוט: משנה קטגוריות',
+        },
+        'ia': {
+                'catall-changing': u'Bot: Alteration de categorias',
+        },
+        'is': {
+                'catall-changing': u'Vélmenni: Breyti flokkum',
+        },
+        'it': {
+                'catall-changing': u'Bot: Cambio categorie',
+        },
+        'ja': {
+                'catall-changing': u'ロボットによる: カテゴリ変更',
+        },
+        'ksh': {
+                'catall-changing': u'Bot: Saachjruppe tuusche of dobei donn',
+        },
+        'lt': {
+                'catall-changing': u'robotas: Keičiamos kategorijos',
+        },
+        'nl': {
+                'catall-changing': u'Bot: wijziging van categorieën',
+        },
+        'pl': {
+                'catall-changing': u'Bot: Zmiana kategorii',
+        },
+        'pt': {
+                'catall-changing': u'Bot: Categorizando',
+        },
+        'sr': {
+                'catall-changing': u'Bot: Ð˜Ð·Ð¼ÐµÐ½Ð° ÐºÐ°Ñ‚ÐµÐ³Ð¾Ñ€Ð¸Ñ˜Ð°',
+        },
+        'sv': {
+                'catall-changing': u'Bot: Ändrar kategori',
+        },
+        'zh': {
+                'catall-changing': u'機器人: 更改分類',
+        },
+}
