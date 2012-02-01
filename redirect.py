8e055a9d776de02c92f737b983dc868d1af8e76a
xqt <xqt@users.mediawiki.org>
2012-02-01 12:27:40 +0000

    wrong sd template replaced for pl-wiki, bug #3482141
    https://mediawiki.org/wiki/Special:Code/pywikipedia/9851

--- redirect.py
+++ redirect.py
@@ -466,7 +466,7 @@ msg = {
 	},
 	# Author: Sp5uhe
 	'pl': {
-		'redirect-broken-redirect-template': u'{{db-r1}}',
+		'redirect-broken-redirect-template': u'{{ek|przekierowanie do usuniętej lub nieistniejącej stron}}',
 		'redirect-fix-double': u'Robot naprawił podwójne przekierowanie do %(to)s',
 		'redirect-remove-broken': u'[[WP:CSD#G8|G8]] – [[Wikipedia:Redirect|przekierowanie]] do usuniętej lub nieistniejącej strony',
 		'redirect-remove-loop': u'[[WP:CSD#G8|G8]] – pętla [[Wikipedia:Redirect|przekierowań]]',
