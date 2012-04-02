e3b839d6f2bd07c2a06293afcc1e8edfdfc5d1ce
Bináris <binbot@users.mediawiki.org>
2012-04-02 13:11:04 +0000

    i18n for ndashredir.py
    https://mediawiki.org/wiki/Special:Code/pywikipedia/10078

---
+++ ndashredir.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+msg = {
+	# Author: Bináris
+	'en': {
+		'ndashredir-create': u'Bot: Creating hyphenated redirect to [[%(title)s]]',
+	},
+	# Author: Bináris
+	'hu': {
+		'ndashredir-create': u'Kiskötőjeles átirányítás létrehozása a(z) [[%(title)s]] lapra (automatikus botszerkesztés)',
+	},
+}
