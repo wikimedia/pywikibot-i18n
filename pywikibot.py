eed70f7ffc369cc6c92f7668b7d08b97f580a463
xqt <xqt@users.mediawiki.org>
2012-04-18 14:40:22 +0000

    The first messages for user interface
    https://mediawiki.org/wiki/Special:Code/pywikipedia/10133

---
+++ pywikibot.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+msg = {
+	'de': {
+		'pywikibot-enter-category-name': u'Bitte Name der Kategorie eingeben:',
+		'pywikibot-enter-file-links-processing': u'Links welcher Dateiseite sollen bearbeitet werden?',
+		'pywikibot-enter-finished-browser': u'Enter drücken nach dem Beenden des Browsers.',
+		'pywikibot-enter-namespace-number': u'Bitte gib die Nummer des Namensraums ein:',
+		'pywikibot-enter-new-text': u'Bitte gib den neuen Text ein: ',
+		'pywikibot-enter-page-processing': u'Welche Seite soll bearbeitet werden?',
+		'pywikibot-enter-xml-filename': u'Bitte gib den Datenamen des XML-Dumps ein:',
+	},
+	'en': {
+		'pywikibot-enter-category-name': u'Please enter the category name:',
+		'pywikibot-enter-file-links-processing': u'Links to which image page should be processed?',
+		'pywikibot-enter-finished-browser': u'Press Enter when finished in browser.',
+		'pywikibot-enter-namespace-number': u'Please enter a namespace by its number:',
+		'pywikibot-enter-new-text': u'Please enter the new text:',
+		'pywikibot-enter-page-processing': u'Which page should be processed?',
+		'pywikibot-enter-xml-filename': u'Please enter the XML dump\'s filename:',
+	},
+	'qqq': {
+		'pywikibot-enter-category-name': u'Message displayed to the bot owner to enter the category name.',
+		'pywikibot-enter-file-links-processing': u'Question displayed to the bot owner processing links to a given image page.',
+		'pywikibot-enter-finished-browser': u'Message displayed to the bot owner to press Enter button when browser edits are finished.',
+		'pywikibot-enter-namespace-number': u'Message displayed to the bot owner to enter a namespace by its number.',
+		'pywikibot-enter-new-text': u'Message displayed to the bot owner to enter the new text.',
+		'pywikibot-enter-page-processing': u'Question displayed to the bot owner which page should be processed.',
+		'pywikibot-enter-xml-filename': u'Message displayed to the bot owner to enter the XML dump\'s filename.',
+	},
+}
