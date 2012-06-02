bd76d17c0acb5a1a3eda53ca0855085c3e03cac7
xqt <xqt@users.mediawiki.org>
2012-06-02 22:59:16 +0000

    i18n messages for spellcheck.py
    https://mediawiki.org/wiki/Special:Code/pywikipedia/10270

---
+++ spellcheck.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+msg = {
+	'ar': {
+		'spellcheck-checking': u'تدقيق إملائي بمساعدة البوت',
+	},
+	'de': {
+		'spellcheck-checking': u'Bot-aided spell checker',
+	},
+	'en': {
+		'spellcheck-checking': u'Bot-aided spell checker',
+	},
+	'es': {
+		'spellcheck-checking': u'Bot asistido de correción ortográfica',
+	},
+	'fr': {
+		'spellcheck-checking': u'Correction orthographique par robot',
+	},
+	'he': {
+		'spellcheck-checking': u'בדיקת איות באמצעות בוט',
+	},
+	'ia': {
+		'spellcheck-checking': u'Correction de orthographia per robot',
+	},
+	'nl': {
+		'spellcheck-checking': u'Spellingscontrole',
+	},
+	'pl': {
+		'spellcheck-checking': u'Wspomagane przez robota sprawdzanie pisowni',
+	},
+	'pt': {
+		'spellcheck-checking': u'Bot de correção ortográfica',
+	},
+}
