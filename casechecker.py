86d4d33ecbefe6370942deeff0817a429b8d794f
xqt <info@gno.de>
2013-12-08 20:46:13 +0100

    (bug 55022) Add casechecker.py to i18n system
    Change-Id: Id086fac369b7e91fd4d309fefec93046fccc7095

---
+++ casechecker.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+msg = {
+	'en': {
+		'casechecker-rename': u'Bot: mixed case rename',
+		'casechecker-delete-reason': u'This redirect used to point to %(target)s',
+		'casechecker-delete-summary': u'Bot: This redirect contains identical looking Cyrillic and Latin letters in its title',
+		'casechecker-replacement-linklist': u'%(source)s→%(target)s',
+		'casechecker-replacement-summary': u'Bot: Case Replacements:',
+	},
+	'qqq': {
+		'casechecker-rename': u'Edit summary when bot moves a redirect with mixed Cyrillic and  Latin letters to a new target',
+		'casechecker-delete-reason': u'Reason for speedy deletion request added to the template when bot marks a redirect for deletion because the redirect contains identical looking Cyrillic and Latin letters in its title\n<code>%(target)s</code> is the redirect target.',
+		'casechecker-delete-summary': u'Edit summary when bot marks a redirect for deletion because the redirect contains identical looking Cyrillic and Latin letters in its title',
+		'casechecker-replacement-linklist': u'A short list pointing from source to target page.\n<code>%(source)s</code> and <code>%(target)s</code> are mediwiki links',
+		'casechecker-replacement-summary': u'Short edit summary when bot edits pages and fix case usage',
+	},
+	'ar': {
+		'casechecker-rename': u'بوت: اسم لحالة مخلوطة',
+		'casechecker-replacement-summary': u'استبدالات الحالة',
+	},
+	'de': {
+		'casechecker-delete-reason': u'Weiterleitung auf %(target)s mit gemischten kyrillischen und lateinischen Zeichen.',
+		'casechecker-delete-summary': u'Bot: Diese Weiterleitung enthält identisch aussehende kyrillische und lateinische Zeichen im Titel',
+		'casechecker-replacement-linklist': u'%(source)s nach %(target)s',  
+	},
+	'ru': {
+		'casechecker-delete-summary': u'Перенаправление на %(target)s содержало смесь кириллицы и латиницы в названии',
+		'casechecker-delete-summary': u'бот: Перенаправление содержит смесь кириллицы и латиницы в названии',
+	},
+};
