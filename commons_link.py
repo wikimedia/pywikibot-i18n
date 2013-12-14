7b151cc22adcab054a6dd35cd4d7a8b4867d77e4
GEOFBOT <geofbot@gmail.com>
2013-12-14 19:36:16 +0000

    i18n for commons_link.py
    Change-Id: Ic3110206558c1168765eb3b1e71dd7f57f19bb46

---
+++ commons_link.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+msg = {
+	'en': {
+		'commons_link-template-added': u'Robot: Include commons template',
+		'commons_link-cat-template-added': u'Robot: Include commonscat template',
+	},
+	'qqq': {
+		'commons_link-template-added': u'Edit summary when the bot adds a commons template to a page.',
+		'commons_link-cat-template-added': u'Edit summary when the bot adds a commonscat template to a page.',
+	},
+	'ar': {
+		'commons_link-template-added': u'روبوت: تضمين قالب كومنز',
+		'commons_link-cat-template-added': u'روبوت: تضمين قالب تصنيف كومنز',
+	},
+	'cs': {
+		'commons_link-template-added': u'Robot přidal šablonu commons',
+		'commons_link-cat-template-added': u'Robot přidal šablonu commonscat',
+	},
+	'fa': {
+		'commons_link-template-added': u'ربات: افزودن الگوی ویکی‌انبار',
+		'commons_link-cat-template-added': u'ربات: افزودن الگوی رده‌بندی ویکی‌انبار',
+	},
+	'he': {
+		'commons_link-template-added': u'בוט: מוסיף תבנית Commons',
+		'commons_link-cat-template-added': u'בוט: מוסיף תבנית Commonscat',
+	},
+	'ja': {
+		'commons_link-template-added': u'ロボットによる: テンプレcommons追加',
+		'commons_link-cat-template-added': u'ロボットによる: テンプレcommonscat追加',
+	},
+	'nl': {
+		'commons_link-template-added': u'Bot: sjabloon commons toegevoegd',
+		'commons_link-cat-template-added': u'Bot: sjabloon commonscat toegevoegd',
+	},
+	'zh': {
+		'commons_link-template-added': u'機器人: 增加commons模板',
+		'commons_link-cat-template-added': u'機器人: 增加commonscat模板',
+	}
+};
