5514de7ecb925a7ad52083f89866ca986cc7f47f
Shardul C <shardul.chiplunkar@gmail.com>
2013-12-24 11:55:16 +0000

    lonelypages.py: added i18n TW page
    Added TranslateWiki page for lonelypages.py.
    See Ic832222ec22c90842388ab78c2220af15a567802 for lonelypages.py script.
    
    Change-Id: I9b3506d1e801909a2b1d25f6d82063cdca195d26

---
+++ lonelypages.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+msg = {
+        'ar': {
+                'lonelypages-comment-add-template': u'بوت: صفحة يتيمة، إضافة قالب',
+                'lonelypages-comment-add-disambig-template': u'بوت: إضافة صفحة توضيح',
+        },
+
+        'ca': {
+                'lonelypages-comment-add-template': u'Bot:Pàgina orfe, afegint plantilla',
+                'lonelypages-comment-add-disambig-template': u'Bot; Afegint una desambiguació',
+        },
+
+        'en': {
+                'lonelypages-comment-add-template': u'Robot: Orphan page, add template',
+                'lonelypages-comment-add-disambig-template': u'Robot: Adding a disambig page',
+        },
+
+        'it': {
+                'lonelypages-comment-add-template': u'[[Project:Bot|Bot]]: Voce orfana, aggiungo template {{O}}',
+                'lonelypages-comment-add-disambig-template': u'[[Project:Bot|Bot]]: Aggiungo una disambigua',
+        },
+
+        'ja': {
+                'lonelypages-comment-add-template': u'ロボットによる: 孤立したページへのテンプレート貼付け',
+                'lonelypages-comment-add-disambig-template': u'ロボットによる: 曖昧さ回避の追加',
+        },
+
+        'zh': {
+                'lonelypages-comment-add-template': u'機器人: 本頁的鏈入頁面太少',
+                'lonelypages-comment-add-disambig-template': u'機器人: 增加消歧義頁面',
+        },
+
+        'qqq': {
+                'lonelypages-comment-add-template': u'Edit summary when adding template to orphan page',
+                'lonelypages-comment-add-disambig-template': u'Edit summary when adding lonely page to disambiguation page',
+        },
+};
