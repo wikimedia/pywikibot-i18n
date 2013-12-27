4e63acf35ab95f7bb3bc72b3b512f0c116e73f18
GEOFBOT <geofbot@gmail.com>
2013-12-27 16:16:58 +0000

    Convert protect.py to i18n
    Change-Id: I83ddec7299f266bb47da7b95fe60c7244b53dadc

---
+++ protect.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+msg = {
+	'en': {
+		'protect-simple': u'Robot: Protecting a list of files.',
+		'protect-category': u'Robot: Protecting all pages from category %(cat)s',
+		'protect-links': u'Robot: Protecting all pages linked from %(page)s',
+		'protect-ref': u'Robot: Protecting all pages referring from %(page)s',
+		'protect-images': u'Robot: Protecting all images on page %(page)s',
+	},
+	# Author: Geoffrey "GEOFBOT" Mon
+	'qqq': {
+		'protect-simple': u'Edit summary when the bot protects a list of files.',
+		'protect-category': u'Edit summary when the bot protects all pages from a category (%(cat)s).',
+		'protect-links': u'Edit summary when the bot protects all pages linked from a page (%(page)s).',
+		'protect-ref': u'Edit summary when the bot protects all pages referring from a page (%(page)s).',
+		'protect-images': u'Edit summary when the bot protects all images on a page (%(page)s).',
+	},
+	'ar': {
+		'protect-simple': u'بوت: حماية قائمة من الملفات.',
+		'protect-category': u'روبوت - حماية كل الصفحات من التصنيف %(cat)s',
+		'protect-links': u'روبوت - حماية كل الصفحات الموصولة من %(page)s',
+		'protect-ref': u'روبوت - حماية كل الصفحات الراجعة من %(page)s',
+		'protect-images': u'روبوت - حماية كل الصور في الصفحة %(page)s',
+	},
+	'it': {
+		'protect-simple': u'Bot: Protezione di una lista di pagine.',
+		'protect-category': u'Bot: Protezione di tutte le pagine nella categoria %(cat)s.',
+		'protect-links': u'Bot: Protezione di tutte le pagine linkate da %(page)s.',
+		'protect-ref': u'Bot: Protezione di tutte le pagine con link verso %(page)s.',
+		'protect-images': u'Bot: Protezione di tutte le immagini presenti in %page)s.',
+	},
+	'fa': {
+		'protect-simple': u'ربات:حفاظت فهرستی از صفحه‌ها',
+		'protect-category': u'ربات:حفاظت تمام صفحه‌های رده %(cat)s',
+		'protect-links': u'ربات:حفاظت تمام صفحه‌هایی که در %(page)s پیوند شده‌اند.',
+		'protect-ref': u'ربات:حفاظت تمام صفحه‌هایی که به %(page)s پیوند داده‌اند',
+		'protect-images': u'ربات:حفاظت تمام تصاویر به کار رفته در %(page)s',
+	},
+	'nl': {
+		'protect-simple': u'Bot: lijst met bestanden beveiligd',
+		'protect-category': u'Bot: alle pagina\'s uit categorie %(cat)s beveiligd',
+		'protect-links': u'Bot: alle pagina\'s met verwijzingen vanaf %(page)s beveiligd',
+		'protect-ref': u'Bot: alle pagina\'s met een verwijzing op %(page)s beveiligd',
+		'protect-images': u'Bot: alle bestanden gebruikt op %(page)s beveiligd',
+	},
+	'pt': {
+		'protect-simple': u'Bot: Protegendo uma lista de artigos.',
+		'protect-category': u'Bot: Protegendo todos os artigos da categoria %(cat)s',
+		'protect-links': u'Bot: Protegendo todos os artigos ligados a %(page)s',
+		'protect-ref': u'Bot: Protegendo todos os artigos afluentes a %(page)s',
+		'protect-images': u'Bot: Protegendo todas as imagens do artigo %(page)',
+	},
+	'zh': {
+		'protect-simple': u'機器人:保護清單中的所有檔案',
+		'protect-category': u'機器人: 保護目錄 %(cat)s 的所有頁面',
+		'protect-links': u'機器人: 保護所有從 %(page)s 連結的頁面',
+		'protect-ref': u'機器人: 保護所有連至 %(page)s 的頁面',
+		'protect-images': u'機器人: 保護頁面 %(page)s 中的所有圖條',
+	},
+};
