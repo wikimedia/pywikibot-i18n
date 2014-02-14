d85a0e6a5e7d68151c97fa688d828aefec2fd28a
xqt <info@gno.de>
2014-02-14 18:12:26 +0100

    Add category title as a link inside the message to be more flexible
    - "en" translation on top
    - add doc message
    - add "de" translation
    
    Change-Id: Ia2e3397bc169653c2e0c97387dd49b6e36855631

--- makecat.py
+++ makecat.py
@@ -1,42 +1,48 @@
 # -*- coding: utf-8 -*-
 msg = {
-	'ar': {
-		'makecat-create': u'بوت: إنشاء أو تحديث التصنيف:',
-	},
 	'en': {
-		'makecat-create': u'Robot: Creation or update of category:',
+		'makecat-create': u'Robot: Creation or update of category [[:Category:%(cat)s|]]',
+	},
+	'qqq': {
+		'makecat-create': u'{{doc-important|Do not change ":Category:%(cat)s" so this message will work in any language.}}\nEdit summary when the bot creates or updates a category.\n* %(cat)s - category name',
+	},
+	'ar': {
+		'makecat-create': u'بوت: إنشاء أو تحديث التصنيف [[:Category:%(cat)s|]]',
+	},
+	'de': {
+		'makecat-create': u'Bot: Kategorie [[:Category:%(cat)s|]] wurde neu angelegt oder aktualisiert',
 	},
 	'es': {
-		'makecat-create': u'Bot: Creación o actualiza de la categoría:',
+		'makecat-create': u'Bot: Creación o actualiza de la categoría [[:Category:%(cat)s|]]',
 	},
 	'fa': {
-		'makecat-create': u'ربات: ایجاد یا تصحیح رده:',
+		'makecat-create': u'ربات: ایجاد یا تصحیح رده [[:Category:%(cat)s|]]',
 	},
 	'fr': {
-		'makecat-create': u'Robot : Création ou mise à jour de categorie:',
+		'makecat-create': u'Robot: Création ou mise à jour de categorie [[:Category:%(cat)s|]]',
 	},
 	'he': {
-		'makecat-create': u'בוט: יצירה או עדכון של קטגוריה:',
+		'makecat-create': u'בוט: יצירה או עדכון של קטגוריה [[:Category:%(cat)s|]]',
 	},
 	'ia': {
-		'makecat-create': u'Bot: Creation o actualisation de categoria:',
+		'makecat-create': u'Bot: Creation o actualisation de categoria [[:Category:%(cat)s|]]',
 	},
 	'it': {
-		'makecat-create': u'Bot: La creazione o laggiornamento di categoria:',
+		'makecat-create': u'Bot: La creazione o laggiornamento di categoria [[:Category:%(cat)s|]]',
 	},
 	'nl': {
-		'makecat-create': u'Robot: Aanmaak of uitbreiding van categorie:',
+		'makecat-create': u'Robot: Aanmaak of uitbreiding van categorie [[:Category:%(cat)s|]]',
 	},
 	'nn': {
-		'makecat-create': u'robot: oppretting eller oppdatering av kategori:',
+		'makecat-create': u'robot: oppretting eller oppdatering av kategori [[:Category:%(cat)s|]]',
 	},
 	'no': {
-		'makecat-create': u'Robot: opprettelse eller oppdatering av kategori:',
+		'makecat-create': u'Robot: opprettelse eller oppdatering av kategori [[:Category:%(cat)s|]]',
 	},
 	'pl': {
-		'makecat-create': u'Robot: Stworzenie lub aktualizacja kategorii:',
+		'makecat-create': u'Robot: Stworzenie lub aktualizacja kategorii [[:Category:%(cat)s|]]',
 	},
 	'pt': {
-		'makecat-create': u'Robô: Criando ou atualizando categoria:',
+		'makecat-create': u'Robô: Criando ou atualizando categoria [[:Category:%(cat)s|]]',
 	},
 };
