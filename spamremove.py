6d09c308a0890e9bfd49231ba463b98911deaf2a
xqt <info@gno.de>
2014-03-22 10:28:24 +0100

    i18n for spamremove.py
    Change-Id: I93a6598f8681407814dda4cdc15ce85f6b6a145c

---
+++ spamremove.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+msg = {
+	'ar': {
+		'spamremove-remove': u'إزالة الوصلات إلى موقع سبام %(url)s',
+	},
+	'de': {
+		'spamremove-remove': u'Entferne in Spam-Blacklist eingetragenen Weblink auf %(url)s',
+	},
+	'en': {
+		'spamremove-remove': u'Removing links to spamming site %(url)s',
+	},
+	'es': {
+		'spamremove-remove': u'Removiendo enlaces a sitio publicitario %(url)s',
+	},
+	'fa': {
+		'spamremove-remove': u'حذف پیوند به وبگاه هرزنگاری %(url)s',
+	},
+	'fr': {
+		'spamremove-remove': u'Suppression du lien blacklisté %(url)s',
+	},
+	'he': {
+		'spamremove-remove': u'מסיר קישורים לאתר ספאם %(url)s',
+	},
+	'it': {
+		'spamremove-remove': u'Rimuovo link contenuto nella Spam-Blacklist %(url)s',
+	},
+	'ja': {
+		'spamremove-remove': u'ロボットによる: 迷惑リンク削除 %(url)s',
+	},
+	'nl': {
+		'spamremove-remove': u'Links naar gespamde site: %(url)s verwijderd',
+	},
+	'pt': {
+		'spamremove-remove': u'Removendo links de spam do site %(url)s',
+	},
+	'qqq': {
+		'spamremove-remove': u'Edit summary for spamremove report',
+	},
+	'ta': {
+		'spamremove-remove': u'எரிதமாக இணைக்கப்பட்ட %(url)s இணையத்தளம் நீக்கப்பட்டது',
+	},
+	'vi': {
+		'spamremove-remove': u'xóa các liên kết đến website spam %(url)s',
+	},
+	'zh': {
+		'spamremove-remove': u'機器人: 移除廣告黑名單連結 %(url)s',
+	},
+};
