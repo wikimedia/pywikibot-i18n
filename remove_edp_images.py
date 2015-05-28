9f04ad54a3f9a9d1367433c5470802f3588a114a
xqt <info@gno.de>
2015-05-28 17:50:02 +0200

    change i18n key to "remove_edp_images-edit-summary"
    Change-Id: I886a9114c4aee4bab61bb4b64b91ed075eff2cba

--- remove_edp_images.py
+++ remove_edp_images.py
@@ -3,18 +3,18 @@
 msg = {
     # Author: Shizhao
     'en': {
-        'deledpimage-edit-summary': u'Robot: Fix EDP image use: [[%s]]',
+        'remove_edp_images-edit-summary': u'Robot: Fix EDP image use: [[%s]]',
     },
     # Author: Priyanka Jayaswal
     'qqq': {
-        'deledpimage-edit-summary': u'Edit summary when the bot fixes EDP image use.',
+        'remove_edp_images-edit-summary': u'Edit summary when the bot fixes EDP image use.',
     },
     # Author: Meno25
     'ar': {
-        'deledpimage-edit-summary': u'روبوت: إصلاح استخدام صورة EDP: [[%s]]',
+        'remove_edp_images-edit-summary': u'روبوت: إصلاح استخدام صورة EDP: [[%s]]',
     },
     # Author: Shizhao
     'zh': {
-        'deledpimage-edit-summary': u'Bot修正EDP图像用法：[[%s]]',
+        'remove_edp_images-edit-summary': u'Bot修正EDP图像用法：[[%s]]',
     },
 };
