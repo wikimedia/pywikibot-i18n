7dd81cea84c4d977758002bee54c6bfb567b5f69
Kunal Mehta <legoktm@gmail.com>
2015-08-30 21:31:12 -0700

    Don't hardcode every directory in Gruntfile.js
    See <http://gruntjs.com/configuring-tasks#globbing-patterns>.
    
    Bug: T85335
    Change-Id: Iea4f25b16f87e865ccb4dde63ec90236b8246f08

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,55 +8,7 @@ module.exports = function ( grunt ) {
 
 	grunt.initConfig( {
 		banana: {
-			pywikibot: 'pywikibot/',
-			thirdparty: 'thirdparty/',
-			add_text: 'add_text/',
-			archivebot: 'archivebot/',
-			basic: 'basic/',
-			blockpageschecker: 'blockpageschecker/',
-			capitalize_redirects: 'capitalize_redirects/',
-			casechecker: 'casechecker/',
-			catall: 'catall/',
-			category: 'category/',
-			category_redirect: 'category_redirect/',
-			clean_sandbox: 'clean_sandbox/',
-			commons_link: 'commons_link/',
-			commons: 'commons/',
-			cosmetic_changes: 'cosmetic_changes/',
-			delete: 'delete/',
-			djvutext: 'djvutext/',
-			editarticle: 'editarticle/',
-			featured: 'featured/',
-			fixing_redirects: 'fixing_redirects/',
-			followlive: 'followlive/',
-			interwiki: 'interwiki/',
-			interwikidata: 'interwikidata/',
-			isbn: 'isbn/',
-			lonelypages: 'lonelypages/',
-			makecat: 'makecat/',
-			misspelling: 'misspelling/',
-			movepages: 'movepages/',
-			ndashredir: 'ndashredir/',
-			noreferences: 'noreferences/',
-			pagefromfile: 'pagefromfile/',
-			piper: 'piper/',
-			protect: 'protect/',
-			redirect: 'redirect/',
-			reflinks: 'reflinks/',
-			remove_edp_images: 'remove_edp_images/',
-			replace: 'replace/',
-			revertbot: 'revertbot/',
-			selflink: 'selflink/',
-			solve_disambiguation: 'solve_disambiguation/',
-			spamremove: 'spamremove/',
-			spellcheck: 'spellcheck/',
-			table2wiki: 'table2wiki/',
-			template: 'template/',
-			undelete: 'undelete/',
-			unprotect: 'unprotect/',
-			unlink: 'unlink/',
-			// Missing messages: weblinkchecker: 'weblinkchecker/',
-			welcome: 'welcome/'
+			all: ['*/', '!node_modules/']
 		}
 	} );
 
@@ -64,3 +16,4 @@ module.exports = function ( grunt ) {
 	grunt.registerTask( 'test', ['lint'] );
 	grunt.registerTask( 'default', ['test'] );
 };
+
