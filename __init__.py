e146cb8740ba3bd48f99abd3a93ff9af007100bf
xqt <info@gno.de>
2022-05-14 13:22:16 +0200

    [i18n] Remove coding info
    - remove coding info from __init__.py because utf-8 is default
      in python 3
    - also add licence notes
    
    Change-Id: Id5ef020899ab90a2330a0dee8313568eb9d7b3f5

--- __init__.py
+++ __init__.py
@@ -1,5 +1,11 @@
-# -*- coding: utf-8 -*-
 """
 This is a fake __init__.py to help pkgutil find the i18n data. There
 is no actual python code in this package.
 """
+#
+# (C) Pywikibot team, 2011-2022
+#
+# Distributed under the terms of the MIT license.
+#
+# Translations by translators are licensed CC BY 3.0
+#
