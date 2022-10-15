94317b97620c2f523b1ec9745871a2bcfa405c23
xqt <info@gno.de>
2022-10-15 17:57:25 +0200

    [doc] Update init docstring
    Change-Id: Ia820637f946d44c733758f5bf73e747da1dbc7b7

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
-"""
-This is a fake __init__.py to help pkgutil find the i18n data. There
-is no actual python code in this package.
+"""Folder containing i18n translations bundles.
+
+This file exists to help pkgutil find the i18n data. There is no actual
+python code in this package.
 """
 #
 # (C) Pywikibot team, 2011-2022
